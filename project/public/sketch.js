7c749c9e4861e258c285429c696864b812bd9485
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-05-17 14:05:20 -0400

    pix density test

--- project/public/sketch.js
+++ project/public/sketch.js
@@ -20,7 +20,7 @@ function setup() {
 	if (iOSSafari) {
 		pixelDensity(1.0);
 	} else {
-		pixelDensity(3.0);
+		pixelDensity(1.0);
 	}
 	createCanvas((16 * 300) / 3, (22 * 300) / 3);
 	colorMode(HSB, 360, 100, 100, 100);
