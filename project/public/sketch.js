33f06be8e446e0939d198032a8f8a2228a285edb
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-09-04 15:29:06 -0400

    loop

--- project/public/sketch.js
+++ project/public/sketch.js
@@ -1,8 +1,17 @@
 let features = '';
+let noiseMax = 0;
 
+let phase = 0;
+let phase_inc = 0;
+let max_radius = 0;
+let p_size = 1;
+let hue = 0;
+let sat = 0;
+let brightness = 100;
 function setup() {
 	console.log(features);
 	features = $fx.getFeatures();
+	createCanvas(windowWidth, windowHeight);
 
 	let formatMode = features.format_mode;
 	var ua = window.navigator.userAgent;
@@ -14,28 +23,59 @@ function setup() {
 	if (iOSSafari) {
 		pixelDensity(1.0);
 	} else {
-		pixelDensity(3.0);
+		pixelDensity(4.0);
 	}
-	createCanvas(1500, 1500);
+
 	colorMode(HSB, 360, 100, 100, 100);
 	randomSeed(fxrand() * 10000);
 	noiseSeed(fxrand() * 10000);
-	console.log(features);
+	background(0);
 }
 
 function draw() {
 	// put drawing code here
-	background(0);
-	translate(width / 2, height / 2);
-	stroke(255);
-	noFill();
 
+	if (kname == '32') {
+		noiseMax = map(int(kval), 0, 100, 0, 10, true);
+	}
+	if (kname == '33') {
+		phase_inc = map(int(kval), 0, 100, 0, 0.1, true);
+	}
+	if (kname == '34') {
+		max_radius = map(int(kval), 0, 100, 0, width, true);
+	}
+	if (kname == '35') {
+		p_size = map(int(kval), 0, 100, 0, 2, true);
+	}
+
+	translate(width / 2, height / 2);
+	strokeWeight(1);
+	stroke(hue, sat, brightness, 10);
+	noFill();
+	let vertex_array = [];
+	//let noiseMax = slider.value();
+	//let t = 0;
 	beginShape();
 	for (let a = 0; a < TWO_PI; a += 0.01) {
-		let r = 200;
+		let yoff = map(cos(a - phase), -1, 1, 0, noiseMax);
+		let xoff = map(sin(a + phase), -1, 1, 0, noiseMax);
+		let r = map(noise(xoff, yoff), 0, 1, 0, max_radius);
 		let x = r * cos(a);
 		let y = r * sin(a);
-		vertex(x, y);
+		//vertex(x, y);
+		vertex_array.push({x: x, y: y});
 	}
-	endShape();
+
+	/* 	curveVertex(vertex_array[0].x, vertex_array[0].y);
+	curveVertex(vertex_array[1].x, vertex_array[1].y);
+	curveVertex(vertex_array[2].x, vertex_array[2].y); */
+
+	endShape(CLOSE);
+	phase += phase_inc;
+	for (let i = 0; i < vertex_array.length; i++) {
+		strokeWeight(p_size);
+		stroke(hue, sat, brightness, 10);
+		point(vertex_array[i].x, vertex_array[i].y);
+	}
+	//noLoop();
 }
