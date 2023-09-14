c16475c9503b438b2a20c7daa7d6891e05172071
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-09-14 10:56:31 -0400

    bookmark init

--- project/public/object/mover.js
+++ project/public/object/mover.js
@@ -1,88 +1,90 @@
 class Mover {
-	constructor(x, y, hue, scl1, scl2, ang1, ang2, xMin, xMax, yMin, yMax, isBordered, seed) {
+	constructor(x, y, hue, scl1, scl2, ang1, ang2, xMin, xMax, yMin, yMax, xRandDivider, yRandDivider) {
 		this.x = x;
 		this.y = y;
 		this.initHue = hue;
-		this.initSat = random([0, 20, 40, 60, 80, 100]);
-		this.initBri = random([0, 10, 10, 20, 20, 40, 60, 70, 90]);
+		this.initSat = random([0, 10, 30, 40, 40, 60, 80, 80, 90, 100]);
+		this.initBri = random([0, 10, 20, 20, 40, 40, 60, 70, 80, 90, 100]);
 		this.initAlpha = 50;
-		this.initS = 1;
+		this.initS = 1 * MULTIPLIER;
 		this.hue = this.initHue;
 		this.sat = this.initSat;
 		this.bri = this.initBri;
 		this.a = this.initAlpha;
+		this.hueStep = 20;
+		this.satStep = 1;
+		this.briStep = 1;
 		this.s = this.initS;
 		this.scl1 = scl1;
 		this.scl2 = scl2;
 		this.ang1 = ang1;
 		this.ang2 = ang2;
-		this.seed = seed;
-		this.xRandDivider = 1;
-		this.yRandDivider = 1;
+		this.xRandDivider = 0.1;
+		this.yRandDivider = 0.1;
 		this.xRandSkipper = 0;
 		this.yRandSkipper = 0;
+		this.xRandSkipperVal = 0.1;
+		this.yRandSkipperVal = 0.1;
 		this.xMin = xMin;
 		this.xMax = xMax;
 		this.yMin = yMin;
 		this.yMax = yMax;
-		this.isBordered = isBordered;
+		this.oct = 6;
+		this.centerX = width / 2;
+		this.centerY = height / 2;
+		this.borderX = width / 2;
+		this.borderY = height / 2.75;
+		this.uvalue = 4;
+		this.isBordered = true;
 	}
 
 	show() {
-		//
-		//blendMode(SCREEN);
-
 		fill(this.hue, this.sat, this.bri, this.a);
 		noStroke();
-		rect(this.x, this.y, this.s);
+		rect(this.x, this.y, this.s, this.s);
 	}
 
 	move() {
-		let p = superCurve(this.x, this.y, this.scl1, this.scl2, this.ang1, this.ang2, this.seed);
+		let p = superCurve(this.x, this.y, this.scl1, this.scl2, this.ang1, this.ang2, this.oct);
 
-		/* 		this.xRandDivider = random([0.1, 30, 50, 100]);
-		this.yRandDivider = random([0.1, 30, 50, 100]); */
-		this.xRandDivider = 0.1;
-		this.yRandDivider = 0.1;
-		/* this.xRandDivider = random(0.01, 12);
-		this.yRandDivider = random(0.01, 12); */
-		this.xRandSkipper = random(-0.1, 0.1);
-		this.yRandSkipper = random(-0.1, 0.1);
+		this.xRandSkipper = random(-this.xRandSkipperVal * MULTIPLIER, this.xRandSkipperVal * MULTIPLIER);
+		this.yRandSkipper = random(-this.xRandSkipperVal * MULTIPLIER, this.xRandSkipperVal * MULTIPLIER);
 
-		this.x += p.x / this.xRandDivider + this.xRandSkipper;
-		this.y += p.y / this.yRandDivider + this.yRandSkipper;
+		this.x += (p.x * MULTIPLIER) / this.xRandDivider + this.xRandSkipper;
+		this.y += (p.y * MULTIPLIER) / this.yRandDivider + this.yRandSkipper;
 
-		//shortand for if this.x is less than 0, set this.x to width and vice versa
+		let pxy = p.x - p.y;
+		this.hue += mapValue(pxy, -this.uvalue * 2, this.uvalue * 2, -this.hueStep, this.hueStep, true);
+		this.hue = this.hue > 360 ? 0 : this.hue < 0 ? 360 : this.hue;
+		this.sat += mapValue(p.x, -this.uvalue * 2, this.uvalue * 2, -this.satStep, this.satStep, true);
+		this.sat = this.sat > 100 ? 0 : this.sat < 0 ? 100 : this.sat;
+		this.bri += mapValue(p.y, -this.uvalue * 2, this.uvalue * 2, -this.briStep, this.briStep, true);
+		this.bri = this.bri > 100 ? 0 : this.bri < 0 ? 100 : this.bri;
+
 		this.x = this.x <= 0 ? width - 2 : this.x >= width ? 0 : this.x;
 		this.y = this.y <= 0 ? height - 2 : this.y >= height ? 0 : this.y;
-
-		//let pxy = p.x - p.y;
-
-		//this.a = map(p.x, -4, 4, this.initAlpha - 5, this.initAlpha + 5, true);
-		//this.s = map(p.x, -24, 24, this.initS + 10, this.initS - 10, true);
-		this.hue += map(p.x, -20, 20, -10, 10, true);
-		this.hue = this.hue > 360 ? this.hue - 360 : this.hue < 0 ? this.hue + 360 : this.hue;
-		//this.sat = map(p.x, -2, 2, 0, 20, true);
-		//this.bri = map(p.x, -2, 2, 0, 40, true);
 
 		if (this.isBordered) {
 			if (this.x < (this.xMin - 0.015) * width) {
 				this.x = (this.xMax + 0.015) * width;
+				//this.a = 0;
 			}
 			if (this.x > (this.xMax + 0.015) * width) {
 				this.x = (this.xMin - 0.015) * width;
+				//this.a = 0;
 			}
 			if (this.y < (this.yMin - 0.015) * height) {
 				this.y = (this.yMax + 0.015) * height;
+				//this.a = 0;
 			}
 			if (this.y > (this.yMax + 0.015) * height) {
 				this.y = (this.yMin - 0.015) * height;
+				//this.a = 0;
 			}
 		}
 	}
 }
-
-function superCurve(x, y, scl1, scl2, ang1, ang2, seed) {
+function superCurve(x, y, scl1, scl2, ang1, ang2, octave) {
 	let nx = x,
 		ny = y,
 		a1 = ang1,
@@ -92,23 +94,23 @@ function superCurve(x, y, scl1, scl2, ang1, ang2, seed) {
 		dx,
 		dy;
 
-	dx = oct(nx, ny, scale1, 0);
-	dy = oct(nx, ny, scale2, 2);
+	/* 	dx = oct(nx, ny, scale1, 0, octave);
+	dy = oct(nx, ny, scale2, 2, octave);
 	nx += dx * a1;
 	ny += dy * a2;
 
-	dx = oct(nx, ny, scale1, 1);
-	dy = oct(nx, ny, scale2, 3);
+	dx = oct(nx, ny, scale1, 1, octave);
+	dy = oct(nx, ny, scale2, 3, octave);
 	nx += dx * a1;
 	ny += dy * a2;
 
-	dx = oct(nx, ny, scale1, 1);
-	dy = oct(nx, ny, scale2, 2);
+	dx = oct(nx, ny, scale1, 1, octave);
+	dy = oct(nx, ny, scale2, 2, octave);
 	nx += dx * a1;
-	ny += dy * a2;
+	ny += dy * a2; */
 
-	let un = oct(nx, ny, scale1, 3);
-	let vn = oct(nx, ny, scale2, 2);
+	let un = oct(nx, ny, scale1, 3, octave);
+	let vn = oct(nx, ny, scale2, 2, octave);
 
 	let u = map(un, -0.015, 0.015, -5, 5, true);
 	let v = map(vn, -0.0015, 0.0015, -15, 15, true);
--- project/public/sketch.js
+++ project/public/sketch.js
@@ -1,4 +1,3 @@
-let features = '';
 let movers = [];
 let scl1;
 let scl2;
@@ -10,12 +9,13 @@ let xMin;
 let xMax;
 let yMin;
 let yMax;
-let isBordered = false;
+let startTime;
+let maxFrames = 60;
+let C_WIDTH;
+let MULTIPLIER;
 
 function setup() {
-	features = $fx.getFeatures();
-	console.log(features);
-	let formatMode = features.format_mode;
+	console.time('setup');
 	var ua = window.navigator.userAgent;
 	var iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
 	var webkit = !!ua.match(/WebKit/i);
@@ -25,14 +25,23 @@ function setup() {
 	if (iOSSafari) {
 		pixelDensity(1.0);
 	} else {
-		pixelDensity(3.0);
+		pixelDensity(1.0);
 	}
-	createCanvas((16 * 300) / 3, (22 * 300) / 3);
-	/* 	createCanvas(1080, 1920); */
+	C_WIDTH = min(windowWidth, windowHeight);
+	MULTIPLIER = C_WIDTH / 1600;
+	c = createCanvas(C_WIDTH, C_WIDTH * 3);
+
+	/*
+		window.addEventListener('resize', onResize);
+		onResize();
+		*/
+
+	rectMode(CENTER);
+	randomSeed(seed);
+	noiseSeed(seed);
 	colorMode(HSB, 360, 100, 100, 100);
-	rseed = randomSeed(fxrand() * 10000);
-	nseed = noiseSeed(fxrand() * 10000);
-	INIT(rseed);
+	startTime = frameCount;
+	INIT();
 }
 
 function draw() {
@@ -43,48 +52,80 @@ function draw() {
 			movers[i].move();
 		}
 	}
-	if (frameCount > 100) {
+	let elapsedTime = frameCount - startTime;
+	if (elapsedTime > maxFrames) {
 		noLoop();
-		return;
 	}
 }
 
-function windowResized() {
-	resizeCanvas(windowWidth, windowHeight);
-	INIT(rseed);
-}
+///////////////////////////////////////////////////////
+// -------------------- UTILS ------------------------
+//////////////////////////////////////////////////////
 
-function INIT(seed) {
+function INIT() {
+	console.log('INIT');
+	let hue = random(360);
+	let bgCol = color(random(0, 360), random([0, 2, 5]), 95, 100);
+
+	background(bgCol);
+
+	drawTexture(hue);
 	movers = [];
-	scl1 = random(0.0001, 0.005);
-	scl2 = random(0.0001, 0.005);
-	ang1 = int(random(1000));
-	ang2 = int(random(1000));
+	scl1 = random(0.0001, 0.002);
+	scl2 = random(0.0001, 0.002);
+	ang1 = int(random(5));
+	ang2 = int(random(5));
+
+	let xRandDivider = 0.1;
+	let yRandDivider = xRandDivider;
 
 	xMin = 0.15;
 	xMax = 0.85;
-	yMin = 0.15;
-	yMax = 0.85;
+	yMin = 0.06;
+	yMax = 0.94;
 	/* 	xMin = -0.05;
 	xMax = 1.05;
 	yMin = -0.05;
 	yMax = 1.05; */
-	rectMode(CENTER);
-	let hue = random(360);
-	for (let i = 0; i < 200000; i++) {
-		/* 		// distribue the movers within a circle using polar coordinates
-		let r = randomGaussian(4, 2);
-		let theta = random(0, TWO_PI);
-		let x = width / 2 + r * cos(theta) * 100;
-		let y = height / 2 + r * sin(theta) * 100; */
 
+	for (let i = 0; i < 300000; i++) {
 		let x = random(xMin, xMax) * width;
 		let y = random(yMin, yMax) * height;
 
-		let initHue = hue + random(-10, 10);
+		let initHue = hue + random(-1, 1);
 		initHue = initHue > 360 ? initHue - 360 : initHue < 0 ? initHue + 360 : initHue;
-		movers.push(new Mover(x, y, initHue, scl1, scl2, ang1, ang2, xMin, xMax, yMin, yMax, isBordered, seed));
+		movers.push(
+			new Mover(
+				x,
+				y,
+				initHue,
+				scl1 / MULTIPLIER,
+				scl2 / MULTIPLIER,
+				ang1 * MULTIPLIER,
+				ang2 * MULTIPLIER,
+				xMin,
+				xMax,
+				yMin,
+				yMax,
+				xRandDivider,
+				yRandDivider
+			)
+		);
 	}
-	let bgCol = spectral.mix('#fff', '#000', 0.138);
-	background(bgCol);
+}
+
+function drawTexture(hue) {
+	// draw 200000 small rects to create a texture
+	console.log('drawTexture');
+	for (let i = 0; i < 600000; i++) {
+		let x = random(width);
+		let y = random(height);
+		let sw = 0.45;
+		let h = hue + random(-1, 1);
+		let s = random([0, 20, 40, 60, 80, 100]);
+		let b = random([0, 10, 10, 20, 20, 40, 60, 70, 90, 90, 100]);
+		fill(h, s, b, 100);
+		noStroke();
+		rect(x, y, sw);
+	}
 }
