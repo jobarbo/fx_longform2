34c06073233a93d384c075597a8ab018bc022c76
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-10-04 13:29:34 -0400

    added colours but ang and scl is not agnostic

--- project/public/object/mover.js
+++ project/public/object/mover.js
@@ -6,12 +6,13 @@ class Mover {
 		this.initSat = random([0, 10, 20]);
 		this.initBri = random([0, 10, 20, 20, 40]);
 		this.initAlpha = 100;
-		this.initS = 0.5 * MULTIPLIER;
+		this.initS = 0.6 * MULTIPLIER;
 		this.hue = this.initHue;
+		this.hueArr = [0, 20, 30, 120, 35, 45, 185, 190, 195];
 		this.sat = this.initSat;
 		this.bri = this.initBri;
 		this.a = this.initAlpha;
-		this.hueStep = 10;
+		this.hueStep = 0;
 		this.satStep = 0;
 		this.briStep = 0;
 		this.s = this.initS;
@@ -51,24 +52,26 @@ class Mover {
 	}
 
 	move() {
-		// make ang1 smaller when around the center of the canvas both horizontally and vertically using mapValue
 		let distFromCenter = int(dist(this.x, this.y, this.centerX, this.centerY));
-
-		this.ang1 = int(map(distFromCenter, 0, 400, this.ang1Init / 15, this.ang1Init * 3, true));
-		this.ang2 = int(map(distFromCenter, 0, 400, this.ang1Init / 15, this.ang1Init * 3, true));
-		this.scl1 = map(distFromCenter, 0, 500, 0.0002, 0.005, true);
-		this.scl2 = map(distFromCenter, 0, 500, 0.0002, 0.005, true);
+		//! CHECK WHY ANG AND SCL IS NOT AGNOSTIC TO MULTIPLIER
+		this.ang1 = int(map(distFromCenter, 0, 400 * MULTIPLIER, this.ang1Init / 15, this.ang1Init * 3, true));
+		this.ang2 = int(map(distFromCenter, 0, 400 * MULTIPLIER, this.ang1Init / 15, this.ang1Init * 3, true));
+		this.scl1 = map(distFromCenter, 0, 500 * MULTIPLIER, 0.0002, 0.005, true);
+		this.scl2 = map(distFromCenter, 0, 500 * MULTIPLIER, 0.0002, 0.005, true);
+		//! CHECK WHY ANG AND SCL IS NOT AGNOSTIC TO MULTIPLIER
 		let p = superCurve(this.x, this.y, this.scl1, this.scl2, this.ang1, this.ang2, this.oct);
 
 		this.xRandSkipper = random(-this.xRandSkipperVal * MULTIPLIER, this.xRandSkipperVal * MULTIPLIER);
 		this.yRandSkipper = random(-this.xRandSkipperVal * MULTIPLIER, this.xRandSkipperVal * MULTIPLIER);
-		this.xRandDivider = random(0.0000001, 1);
-		this.yRandDivider = random(0.0000001, 1);
 		this.x += (p.x * MULTIPLIER) / this.xRandDivider + this.xRandSkipper;
 		this.y += (p.y * MULTIPLIER) / this.yRandDivider + this.yRandSkipper;
 
 		let pxy = p.x - p.y;
-		this.hue += mapValue(pxy, -this.uvalue * 2, this.uvalue * 2, -this.hueStep, this.hueStep, true);
+
+		/* 		let hueIndex = int(map(p.x, -4, 4, this.hueArr.length - 1, 0, true));
+
+		this.hue = this.hueArr[hueIndex]; */
+		this.hue += mapValue(pxy, -this.uvalue * 2, this.uvalue * 2, this.hueStep, -this.hueStep, true);
 		this.hue = this.hue > 360 ? 0 : this.hue < 0 ? 360 : this.hue;
 		this.sat += mapValue(pxy, -this.uvalue * 2, this.uvalue * 2, -this.satStep, this.satStep, true);
 		this.sat = this.sat > 100 ? 0 : this.sat < 0 ? 100 : this.sat;
@@ -126,8 +129,8 @@ function superCurve(x, y, scl1, scl2, ang1, ang2, octave) {
 	let un = oct(nx, ny, scale1, 3, octave);
 	let vn = oct(nx, ny, scale2, 2, octave);
 
-	let u = map(un, -0.0000000000015, 0.0000000000015, -1, 1, true);
-	let v = map(vn, -0.0000000000015, 0.0000000000015, -1, 1, true);
+	let u = map(un, -0.0000000000015, 0.0000000000015, -4, 4, true);
+	let v = map(vn, -0.0000000000015, 0.0000000000015, -4, 4, true);
 
 	let p = createVector(u, v);
 	return p;
