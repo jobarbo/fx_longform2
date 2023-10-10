e03254e7e7df5496bbb5823420f10f072117d672
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-10-10 16:51:21 -0400

    path following

---
+++ project/public/objects/vehicle.js
@@ -0,0 +1,48 @@
+class Vehicle {
+	constructor(x, y) {
+		this.pos = createVector(x, y);
+		this.prevPos = createVector(x, y);
+		this.vel = createVector(0, 0);
+		this.acc = createVector(0, 0);
+		this.maxSpeed = 2;
+		this.maxForce = 0.2;
+		this.r = 1;
+		this.a = 100;
+	}
+
+	seek(target) {
+		let desired = p5.Vector.sub(target, this.pos);
+		desired.setMag(this.maxSpeed);
+		let steering = p5.Vector.sub(desired, this.vel);
+		console.log(steering);
+		steering.limit(this.maxForce);
+		this.applyForce(steering);
+	}
+
+	applyForce(force) {
+		this.acc.add(force);
+	}
+
+	update() {
+		this.prevPos = this.pos.copy();
+		this.vel.add(this.acc);
+		this.pos.add(this.vel);
+		this.acc.set(0, 0);
+		// make the alpha higher when closer to the target
+		this.a = map(this.pos.dist(target), 20, 100, 255, 200);
+	}
+
+	show() {
+		stroke(255, this.a);
+		strokeWeight(2);
+		fill(255);
+		push();
+		translate(this.pos.x, this.pos.y);
+		rotate(this.vel.heading());
+		//triangle(-this.r, -this.r / 2, -this.r, this.r / 2, this.r, 0);
+
+		pop();
+		strokeWeight(0.25);
+		line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
+	}
+}
--- project/public/sketch.js
+++ project/public/sketch.js
@@ -1,72 +1,47 @@
-let edge_arr = [];
-let node_arr = [];
-let edge_num = 1;
-let node_num = 30;
+let DEFAULT_SIZE = 3600;
+let W = window.innerWidth;
+let H = window.innerHeight;
+let DIM;
+let MULTIPLIER;
 
-let edge_length = 400;
+let vehicles = [];
 
 function setup() {
-	createCanvas(1000, 1000);
-	pixelDensity(3);
-	colorMode(HSB, 360, 100, 100, 100);
-	angleMode(DEGREES);
-	background(0, 10, 100, 100);
+	console.time('setup');
+	var ua = window.navigator.userAgent;
+	var iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
+	var webkit = !!ua.match(/WebKit/i);
+	var iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
 
-	let edge_start = createVector(width / 2, height / 2);
-	let edge_end = createVector(width / 2, edge_start.y - edge_length);
+	// if safari mobile use pixelDensity(2.0) to make the canvas bigger else use pixelDensity(3.0)
+	if (iOSSafari) {
+		pixelDensity(1.0);
+	} else {
+		pixelDensity(2.0);
+	}
 
-	for (let j = 0; j < node_num; j++) {
-		// distribute nodes along the edge, the first node is at the edge_start and the last node is at the edge_end. All other nodes are evenly distributed.
-		let node_distance = edge_length / (node_num - 1); // Adjusted to include the last node
+	DIM = min(windowWidth, windowHeight);
+	MULTIPLIER = DIM / DEFAULT_SIZE;
+	c = createCanvas(DIM, DIM);
+	background(0);
 
-		// if this is the first node, store the edge_start position, else store the previous node position
-		let prev_node_x = j == 0 ? edge_start.x : node_arr[j - 1].pos.x;
-		let prev_node_y = j == 0 ? edge_start.y : node_arr[j - 1].pos.y;
-		// calculate the current node position based on the previous node position + the distance between nodes
-		let node_x = prev_node_x;
-		let node_y = prev_node_y - node_distance;
-
-		node_arr.push(new Node(node_x, node_y, prev_node_x, prev_node_y, node_distance * j));
+	for (let index = 0; index < 1000; index++) {
+		vehicles[index] = new Vehicle(random(width), random([0, height]));
 	}
 }
 
 function draw() {
-	background(0, 10, 100, 100);
+	//background(0);
+	fill(255, 0, 0);
+	noStroke();
 
-	for (let i = 0; i < edge_num; i++) {
-		for (let j = 0; j < node_arr.length; j++) {
-			node_arr[j].draw();
-			node_arr[j].update();
-		}
+	target = createVector(mouseX, mouseY);
+	//circle(target.x, target.y, 20);
+	blendMode(ADD);
+	for (let vehicle of vehicles) {
+		vehicle.seek(target);
+		vehicle.update();
+		vehicle.show();
 	}
-
-	//ellipse(width / 2, height / 2, 5, 5);
-}
-
-class Node {
-	constructor(x, y, px, py, distance) {
-		// if not the first node, store the previous node position
-		this.prev_node_pos = createVector(px, py);
-		this.pos = createVector(x, y);
-		this.angle = 270;
-		this.distance = distance;
-		this.xoff = random(1000);
-		this.yoff = random(1000);
-	}
-
-	draw() {
-		stroke(0, 0, 0, 100);
-		strokeWeight(1);
-		line(this.prev_node_pos.x, this.prev_node_pos.y, this.pos.x, this.pos.y);
-		stroke(0, 0, 100, 100);
-		strokeWeight(5);
-		point(this.pos.x, this.pos.y);
-	}
-
-	update() {
-		this.prev_pos.x = this.pos.x;
-		this.prev_pos.y = this.pos.y;
-		this.pos.x += random(-1, 1);
-		this.pos.y += random(-1, 1);
-	}
+	blendMode(BLEND);
 }
