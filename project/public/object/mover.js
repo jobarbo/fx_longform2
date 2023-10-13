f1de6efcc9768208f77824451de0f3c6d7523fbb
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-10-13 17:09:07 -0400

    smaller grain

--- project/public/object/mover.js
+++ project/public/object/mover.js
@@ -11,7 +11,7 @@ class Mover {
 				? random([0, 0, 10, 20, 20, 30, 40, 60, 80])
 				: random([40, 60, 70, 70, 80, 80, 80, 90, 100]);
 		this.initAlpha = 100;
-		this.initS = 0.65 * MULTIPLIER;
+		this.initS = 0.45 * MULTIPLIER;
 		this.hue = this.initHue;
 		this.sat = features.colormode === 'monochrome' ? 0 : this.initSat;
 		this.bri = this.initBri;
