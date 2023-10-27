5b7ef8162d7d49d052127c5fe1b7aab10dfc0e7e
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-10-27 18:53:51 -0400

    new main

--- project/public/sketch.js
+++ project/public/sketch.js
@@ -1,39 +1,25 @@
 let features = '';
 
+let DEFAULT_SIZE = 3600;
+let W = window.innerWidth;
+let H = window.innerHeight;
+let DIM;
+let MULTIPLIER;
+
 function setup() {
 	console.log(features);
 	features = $fx.getFeatures();
 
-	let formatMode = features.format_mode;
-	var ua = window.navigator.userAgent;
-	var iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
-	var webkit = !!ua.match(/WebKit/i);
-	var iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
-
-	// if safari mobile use pixelDensity(2.0) to make the canvas bigger else use pixelDensity(3.0)
-	if (iOSSafari) {
-		pixelDensity(1.0);
-	} else {
-		pixelDensity(3.0);
-	}
-	createCanvas(1500, 1500);
+	// canvas setup
+	DIM = min(windowWidth, windowHeight);
+	MULTIPLIER = DIM / DEFAULT_SIZE;
+	c = createCanvas(DIM, DIM * 1.375);
+	dpi(3);
 	colorMode(HSB, 360, 100, 100, 100);
 	randomSeed(fxrand() * 10000);
 	noiseSeed(fxrand() * 10000);
-	console.log(features);
 }
 
 function draw() {
-	// put drawing code here
-	background(255);
-	noStroke();
-	fill(0, 100, 100);
-
-	if (features.shape_type == 'ellipse') {
-		ellipse(mouseX, mouseY, 100, 100);
-	}
-	if (features.shape_type == 'rectangle') {
-		rectMode(CENTER);
-		rect(mouseX, mouseY, 100, 100);
-	}
+	noLoop();
 }
