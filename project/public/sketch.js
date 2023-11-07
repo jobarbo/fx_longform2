6a21a9dd936ee294c4b7e8a36efa0db4257c50e6
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-11-07 12:01:32 -0500

    not working, need fixin

--- project/public/sketch.js
+++ project/public/sketch.js
@@ -9,6 +9,7 @@ let DIM;
 let MULTIPLIER;
 
 let origin;
+let movers = [];
 let moons = [];
 
 let moon_angle = -90;
@@ -44,8 +45,8 @@ function setup() {
 	rotate(moon_angle);
 
 	for (let i = 0; i <= moon_num; i++) {
-		let pos = createVector(0, -DIM * stroke_length);
-		moon_radius = map(i, 0, moon_num, 5, 60);
+		let pos = createVector(0, -height * stroke_length);
+		moon_radius = map(i, 0, moon_num, 15, 100);
 		moons[i] = new Moon(pos.x, pos.y, moon_radius);
 		moons[i].display();
 
@@ -61,7 +62,7 @@ function draw() {
 
 class Origin {
 	constructor() {
-		this.pos = createVector(width / 3, height * 0.7);
+		this.pos = createVector(width / 2.5, height * 0.6);
 		this.r = 10;
 		this.c = color(0, 100, 100, 100);
 		this.debug = false;
@@ -87,6 +88,50 @@ class Moon {
 		this.context = this.graphics.canvas.getContext('2d');
 
 		this.particle_size = 1;
+		this.movers = [];
+		this.init();
+	}
+
+	init() {
+		console.log('INIT');
+		movers = [];
+
+		let xRandDivider = 0.1;
+		let yRandDivider = xRandDivider;
+		let hue = fxrand() * 360;
+		let xMin = 0.05;
+		let xMax = 0.95;
+		let yMin = 0.05;
+		let yMax = 0.95;
+
+		for (let i = 0; i < this.particleNum; i++) {
+			let x = fxrand() * (xMax - xMin) * width + xMin * width;
+			let y = fxrand() * (yMax - yMin) * height + yMin * height;
+
+			let initHue = hue + fxrand() * 2 - 1;
+			initHue = initHue > 360 ? initHue - 360 : initHue < 0 ? initHue + 360 : initHue;
+			movers.push(
+				new Mover(
+					x,
+					y,
+					initHue,
+					scl1 / MULTIPLIER,
+					scl2 / MULTIPLIER,
+					ang1 * MULTIPLIER,
+					ang2 * MULTIPLIER,
+					xMin,
+					xMax,
+					yMin,
+					yMax,
+					xRandDivider,
+					yRandDivider,
+					scl1Zone * MULTIPLIER,
+					scl2Zone * MULTIPLIER,
+					ang1Zone * MULTIPLIER,
+					ang2Zone * MULTIPLIER
+				)
+			);
+		}
 	}
 
 	display() {
@@ -112,12 +157,191 @@ class Moon {
 			let x = random(this.graphics.width);
 			let y = random(this.graphics.height);
 
-			this.graphics.fill(h + random(-vary, vary), s, b + random(-vary, vary), 2);
+			/* this.graphics.fill(h + random(-vary, vary), s, b + random(-vary, vary), 2);
 
 			// make particle size relative to the size of the moon (r)
-			this.particle_size = map(this.r, 5, 60, 1, 12);
-			this.graphics.circle(x, y, this.particle_size);
+			this.particle_size = map(this.r, 5, 60, 1, 24);
+			this.graphics.circle(x, y, this.particle_size); */
+
+			const mover = movers[i];
+			mover.show();
+			mover.move();
 		}
 		image(this.graphics, this.pos.x - this.r / 2, this.pos.y - this.r / 2);
 	}
+}
+
+class Mover {
+	constructor(
+		x,
+		y,
+		hue,
+		scl1,
+		scl2,
+		ang1,
+		ang2,
+		xMin,
+		xMax,
+		yMin,
+		yMax,
+		xRandDivider,
+		yRandDivider,
+		scl1Zone,
+		scl2Zone,
+		ang1Zone,
+		ang2Zone
+	) {
+		this.x = x;
+		this.y = y;
+		this.initHue = parseInt(hue);
+		this.initSat = [0, 0, 10, 20][Math.floor(fxrand() * 4)];
+		this.initBri = [0, 0, 10, 20][Math.floor(fxrand() * 4)];
+		this.initAlpha = 100;
+		this.initS = 0.5 * MULTIPLIER;
+		this.s = this.initS;
+		this.hue = this.initHue;
+		this.sat = this.initSat;
+		this.bri = this.initBri;
+		this.a = this.initAlpha;
+		this.hueStep = 0.0001;
+		this.satStep = 0.0001;
+		this.briStep = 0.0001;
+		this.scl1Init = scl1;
+		this.scl2Init = scl2;
+		this.scl1 = scl1;
+		this.scl2 = scl2;
+		this.ang1Init = ang1;
+		this.ang2Init = ang2;
+		this.ang1 = ang1;
+		this.ang2 = ang2;
+		this.xRandDivider = xRandDivider;
+		this.yRandDivider = yRandDivider;
+		this.xRandSkipper = 0;
+		this.yRandSkipper = 0;
+		this.xRandSkipperVal = 0.1;
+		this.yRandSkipperVal = 0.1;
+		this.xMin = xMin;
+		this.xMax = xMax;
+		this.yMin = yMin;
+		this.yMax = yMax;
+		this.xLimit = 0.00015;
+		this.yLimit = 0.00015;
+		this.oct = 6;
+		this.centerX = width / 2;
+		this.centerY = height / 2;
+		this.borderX = width / 2;
+		this.borderY = height / 2.75;
+		this.uvalue = 4;
+		this.isBordered = true;
+
+		this.ang1Zone = ang1Zone;
+		this.ang2Zone = ang2Zone;
+		this.scl1Zone = scl1Zone;
+		this.scl2Zone = scl2Zone;
+
+		this.ns = 0.5;
+	}
+
+	show() {
+		// draw a pixel
+		drawingContext.fillStyle = `hsla(${this.hue}, ${this.sat}%, ${this.bri}%, ${this.a}%)`;
+		drawingContext.fillRect(this.x, this.y, this.s, this.s);
+	}
+
+	move() {
+		// get the distance from the particle to the chosen location using the sdf_box function (signed distance function).
+		// the sdf_box function returns the distance from the particle to the chosen location.
+		// the sdf_box function takes 3 arguments: the particle's x and y coordinates, the chosen location's x and y coordinates, and the chosen location's width and height.
+		let distFromCenter = sdf_box(
+			[this.x, this.y],
+			[this.centerX, height - 2000 * MULTIPLIER],
+			[4000 * MULTIPLIER, 40 * MULTIPLIER]
+		);
+		let distCircle = sdf_circle([this.x, this.y], [this.centerX, this.centerY + 1700 * MULTIPLIER], 2200 * MULTIPLIER);
+		// smoothstep the distance from the particle to the chosen location.
+
+		//! CHECK WHY ANG AND SCL IS NOT AGNOSTIC TO MULTIPLIER
+		this.ang1 = parseInt(
+			mapValue(distCircle, -100 * MULTIPLIER, 700 * MULTIPLIER, -this.ang1Init / 3, this.ang1Init * 4)
+		);
+		this.ang2 = parseInt(
+			mapValue(distCircle, -100 * MULTIPLIER, 700 * MULTIPLIER, -this.ang2Init / 3, this.ang2Init * 4)
+		);
+		this.scl1 = map(distCircle, -700 * MULTIPLIER, 200 * MULTIPLIER, -this.scl1Init * 3, this.scl1Init, true);
+		this.scl2 = map(distCircle, -700 * MULTIPLIER, 200 * MULTIPLIER, -this.scl2Init * 3, this.scl2Init, true);
+
+		//this.oct = parseInt(map(distCircle, 0, 1, 1, 6, true));
+
+		//this.ang2 = parseInt(map(distFromCenter, 0, this.ang2Zone, this.ang2Init * 2, this.ang2Init / 100, true));
+		/*
+		this.ang1 = parseInt(map(distFromCenter, 0, this.ang1Zone, this.ang1Init / 1000, this.ang1Init * 2, true));
+		this.ang2 = parseInt(map(distFromCenter, 0, this.ang2Zone, this.ang2Init / 1000, this.ang2Init * 2, true));
+		this.scl1 = map(distFromCenter, 0, this.scl1Zone, this.scl1Init / 1000, this.scl1Init * 3, true);
+		this.scl2 = map(distFromCenter, 0, this.scl2Zone, this.scl2Init / 1000, this.scl2Init * 3, true); */
+		//! CHECK WHY ANG AND SCL IS NOT AGNOSTIC TO MULTIPLIER
+		let p = superCurve(this.x, this.y, this.scl1, this.scl2, this.ang1, this.ang2, this.oct, this.ns);
+		this.xRandDivider = fxrand() * 6;
+		this.yRandDivider = fxrand() * 6;
+
+		this.x += (p.x * MULTIPLIER) / this.xRandDivider + this.xRandSkipper;
+		this.y += (p.y * MULTIPLIER) / this.yRandDivider + this.yRandSkipper;
+
+		if (this.x < (this.xMin - this.xLimit) * width) {
+			this.x = (this.xMax + this.xLimit) * width;
+		}
+		if (this.x > (this.xMax + this.xLimit) * width) {
+			this.x = (this.xMin - this.xLimit) * width;
+		}
+		if (this.y < (this.yMin - this.yLimit) * height) {
+			this.y = (this.yMax + this.yLimit) * height;
+		}
+		if (this.y > (this.yMax + this.yLimit) * height) {
+			this.y = (this.yMin - this.yLimit) * height;
+		}
+	}
+}
+
+function superCurve(x, y, scl1, scl2, ang1, ang2, octave, ns) {
+	let nx = x,
+		ny = y,
+		a1 = ang1,
+		a2 = ang2,
+		scale1 = scl1,
+		scale2 = scl2,
+		noiseSpeed = ns,
+		dx,
+		dy;
+
+	dx = oct(nx, ny, scale1, 0, octave);
+	dy = oct(nx, ny, scale2, 2, octave);
+	nx += dx * a1;
+	ny += dy * a2;
+
+	dx = oct(nx, ny, scale1, 1, octave);
+	dy = oct(nx, ny, scale2, 3, octave);
+	nx += dx * a1;
+	ny += dy * a2;
+
+	dx = oct(nx, ny, scale1, 1, octave);
+	dy = oct(nx, ny, scale2, 2, octave);
+	nx += dx * a1;
+	ny += dy * a2;
+
+	let un = oct(nx, ny, scale1, 3, octave);
+	let vn = oct(nx, ny, scale2, 2, octave);
+
+	/* 	let u = clamp(un + 0.5, 0, 1) * 21 - 1;
+	let v = clamp(vn + 0.5, 0, 1) * 21 - 20; */
+
+	let rangeA = [10, 15, 20];
+	let rangeB = [1, 2, 3];
+
+	let aValue = rangeA[Math.floor(fxrand() * rangeA.length)];
+	let bValue = rangeB[Math.floor(fxrand() * rangeB.length)];
+
+	let u = mapValue(un, -noiseSpeed, noiseSpeed, -aValue, bValue);
+	let v = mapValue(vn, -noiseSpeed, noiseSpeed, -bValue, aValue);
+
+	//let p = createVector(u, v);
+	return {x: u, y: v};
 }
