347c5fd9e2134e6154316789cb8a76c394649c9d
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-11-09 00:25:56 -0500

    update HM

--- project/public/object/mover.js
+++ project/public/object/mover.js
@@ -3,10 +3,15 @@ class Mover {
 		this.x = x;
 		this.y = y;
 		this.hue = hue;
-		this.sat = 50;
-		this.bri = 10;
+		this.sat = random([0, 10, 20]);
+		this.bri = random([0, 5, 5, 10, 20]);
 		//this.s = random(random(random(random(min(width, height) * 0.01)))) + 1;
-		this.s = 0.1;
+		this.s = 0.15;
+		this.a = 100;
+		this.xRandDivider = random(0.0001, 2);
+		this.yRandDivider = random(0.00001, 1.1);
+		this.xRandSkipper = 0;
+		this.yRandSkipper = 0;
 		this.scl1 = scl1;
 		this.scl2 = scl2;
 		this.seed = seed;
@@ -15,21 +20,25 @@ class Mover {
 	show() {
 		//
 		//blendMode(SCREEN);
-		fill(this.hue, this.sat, this.bri, 100);
+		fill(this.hue, this.sat, this.bri, this.a);
 		noStroke();
 		rect(this.x, this.y, this.s);
 	}
 
 	move() {
-		let p = superCurve(this.x, this.y, this.scl1, this.scl2, this.seed);
+		let p = superCurve(this.x, this.y, this.scl1, this.scl2, 250, 250, 6, 1);
 
 		//this.hue = map(p.x, -4, 4, this.hue - 3, this.hue + 3, true);
 		//this.sat = map(p.x, -4, 4, this.sat + 2, this.sat - 2, true);
 		//this.bri = map(p.x, -4, 4, this.bri - 2, this.bri + 2, true);
-
-		this.x += p.x / random(0.0001, 2) + random(-0.1, 0.1);
-		this.y += p.y / random(0.00001, 1.1) + random(0.1, 0.1);
-		this.s = map(p.x, -4, 4, 0.1, 0.5);
+		this.xRandDivider = random(0.0001, 2);
+		this.yRandDivider = random(0.00001, 1.5);
+		this.xRandSkipper = random(-0, 0);
+		this.yRandSkipper = random(0, 0);
+		this.x += p.x / this.xRandDivider + this.xRandSkipper;
+		this.y += p.y / this.yRandDivider + this.yRandSkipper;
+		this.a = map(abs(p.x + p.y), 1.99, 2, 50, 100, true);
+		this.s = map(abs(p.x + p.y), 1.99, 2, 0.45, 0.25, true);
 
 		/* 		if (this.hue < 0) {
 			this.hue = 360;
@@ -46,20 +55,54 @@ class Mover {
 		} else if (this.bri < 0) {
 			this.bri = 0;
 		} */
-		if (this.s < 1) {
-			this.s = 1;
-		}
+
 		if (this.s > 10) {
 			this.s = 10;
 		}
 	}
 }
 
-function superCurve(x, y, scl1, scl2, seed) {
-	let u = map(noise(x * scl1, y * scl1, seed), 0, 1, -4, 4);
-	let v = map(noise(x * scl2, y * scl2, seed), 0, 1, 0, 4);
-	//let u = sin(y * scl1 + seed) + cos(y * scl2 + seed) + sin(y * scl2 * 0.2 + seed);
-	//let v = sin(x * scl1 + seed) + cos(x * scl2 + seed) - sin(x * scl2 * 0.2 + seed);
-	let p = createVector(u, v);
-	return p;
+function superCurve(x, y, scl1, scl2, ang1, ang2, octave, ns) {
+	let nx = x,
+		ny = y,
+		a1 = ang1,
+		a2 = ang2,
+		scale1 = scl1,
+		scale2 = scl2,
+		noiseSpeed = ns,
+		dx,
+		dy;
+
+	dx = oct(nx, ny, scale1, 0, octave);
+	dy = oct(nx, ny, scale2, 2, octave);
+	nx += dx * a1;
+	ny += dy * a2;
+
+	dx = oct(nx, ny, scale1, 1, octave);
+	dy = oct(nx, ny, scale2, 3, octave);
+	nx += dx * a1;
+	ny += dy * a2;
+
+	dx = oct(nx, ny, scale1, 1, octave);
+	dy = oct(nx, ny, scale2, 2, octave);
+	nx += dx * a1;
+	ny += dy * a2;
+
+	let un = oct(nx, ny, scale1, 3, octave) + 0.5;
+	let vn = oct(nx, ny, scale2, 2, octave) + 0.5;
+
+	/* 	let u = clamp(un, 0, 1) * 21 - 20;
+	let v = clamp(vn, 0, 1) * 21 - 1; */
+
+	let rangeA = [1, 2, 3];
+	let rangeB = [1, 3, 5];
+
+	let aValue = rangeA[Math.floor(fxrand() * rangeA.length)];
+	let bValue = rangeB[Math.floor(fxrand() * rangeB.length)];
+
+	let u = mapValue(un, 0, noiseSpeed, -aValue, aValue);
+	let v = mapValue(vn, 0, noiseSpeed, 1, bValue);
+
+	//let p = createVector(u, v);
+	return {x: u, y: v};
 }
--- project/public/sketch.js
+++ project/public/sketch.js
@@ -4,44 +4,86 @@ let scl1;
 let scl2;
 let rseed;
 let nseed;
+let startTime;
+let frameIterator = 0;
+let currentFrame = 0;
+
+//let maxFrames = 20;
+let maxFrames = 64;
+//let maxFrames = 64 * 120;
+//let particleNum = 800000;
+let particleNum = 100000;
+//let particleNum = 2250;
+
+// viewport
+let DEFAULT_SIZE = 3600;
+let W = window.innerWidth;
+let H = window.innerHeight;
+let DIM;
+let MULTIPLIER;
+
+let elapsedTime = 0;
+let drawing = true;
+let renderMode = 1;
+let cycle = (maxFrames * particleNum) / 1000;
+
 function setup() {
 	console.log(features);
 	features = $fx.getFeatures();
 
-	let formatMode = features.format_mode;
-	var ua = window.navigator.userAgent;
-	var iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
-	var webkit = !!ua.match(/WebKit/i);
-	var iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
-
-	// if Safari mobile or any smartphone browser, use pixelDensity(0.5) to make the canvas bigger, else use pixelDensity(3.0)
-	if (iOSSafari || (iOS && !iOSSafari) || (!iOS && !ua.match(/iPad/i) && ua.match(/Mobile/i))) {
-		pixelDensity(2);
-	} else {
-		pixelDensity(3);
-	}
-	createCanvas(22 * 100, 16 * 100);
+	pixelDensity(dpi(2));
+	DIM = min(windowWidth, windowHeight);
+	MULTIPLIER = DIM / DEFAULT_SIZE;
+	c = createCanvas(DIM * 1.4, DIM);
 	colorMode(HSB, 360, 100, 100, 100);
 	rseed = randomSeed(fxrand() * 10000);
 	nseed = noiseSeed(fxrand() * 10000);
 	INIT(rseed);
+	let sketch = drawGenerator();
+
+	// use requestAnimationFrame to call the generator function and pass it the sketch function
+	function animate() {
+		//requestAnimationFrame(animate);
+		setTimeout(animate, 0);
+		sketch.next();
+	}
+	animate();
 }
 
-function draw() {
-	// put drawing code here
-	for (let i = 0; i < movers.length; i++) {
-		for (let step = 0; step < 1; step++) {
-			movers[i].show();
-			movers[i].move();
+function* drawGenerator() {
+	let count = 0;
+	let frameCount = 0;
+	let draw_every = cycle;
+
+	// draw the particles and make them move until draw_every is reached then yield and wait for the next frame, also check if the maxFrames is reached and stop the sketch if it is and also show the loading bar
+	while (true) {
+		for (let i = 0; i < movers.length; i++) {
+			const mover = movers[i];
+			if (frameCount > 1) {
+				mover.show();
+			}
+			mover.move();
+			if (count > draw_every) {
+				count = 0;
+				yield;
+			}
+			count++;
+		}
+
+		elapsedTime = frameCount - startTime;
+		//showLoadingBar(elapsedTime, maxFrames, xMin, xMax, yMin, yMax);
+		//drawUI();
+
+		frameCount++;
+
+		if (elapsedTime > maxFrames && drawing) {
+			drawing = false;
+			// close the generator
+			console.timeEnd('setup');
+			return;
 		}
 	}
-
-	if (frameCount > 150) {
-		console.log('done');
-		noLoop();
-	}
 }
-
 /* function windowResized() {
 	resizeCanvas(windowWidth, windowHeight);
 	INIT(rseed);
@@ -53,13 +95,13 @@ function INIT(seed) {
 	scl2 = random(0.003, 0.007);
 	let hue = random(360);
 	let y = random(height / 1.5, height / 3);
-	let x = -100;
-	for (let i = 0; i < 20000; i++) {
+	let x = -400;
+	for (let i = 0; i < particleNum; i++) {
 		// make x iterate from 0 to width with a step of 20 pixels
-		x += random(0.25);
+		x = random(-400, width + 400);
 		// make y start at height/2 but every other steps it's position is affected by noise
 
-		y += map(noise(x * 0.015, seed), 0, 1, -0.25, 0.25);
+		y = map(noise(x * (width / (particleNum * 10)), seed), 0, 1, height / 1.35, height / 6.8, true);
 
 		movers.push(new Mover(x, y, hue, scl1, scl2, seed));
 	}
