ce186a6e8e63c82da3866f26912acdf0cc7a39df
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-11-23 17:52:05 -0500

    moved index.js to top of sketch.js + somewhat working fetch

--- project/public/sketch.js
+++ project/public/sketch.js
@@ -2,6 +2,39 @@
 TAU = PI * 2;
 F = (N, f) => [...Array(N)].map((_, i) => f(i));
 
+console.log(fxhash);
+
+const sp = new URLSearchParams(window.location.search);
+//console.log(sp);
+
+let composition_params;
+
+composition_params = generate_composition_params();
+//console.log(composition_params);
+
+var {complexity, theme, composition, colormode, strokestyle, clampvalue} = composition_params; // unpacking parameters we need in main.js and turning them into globals
+
+//console.log(shape_type, ellipse_num, line_num, rectangle_num, bg_mode, border_mode, format_mode, palette_mode, angle_mode);
+// this is how to define parameters
+// this is how to define parameters
+//console.log(theme);
+// this is how features can be defined
+$fx.features({
+	complexity: complexity,
+	theme: theme,
+	composition: composition,
+	colormode: colormode,
+	strokestyle: strokestyle,
+	clampvalue: clampvalue,
+});
+
+// log the parameters, for debugging purposes, artists won't have to do that
+//console.log('Current param values:');
+
+// Added addtional transformation to the parameter for easier usage
+// e.g. color.hex.rgba, color.obj.rgba.r, color.arr.rgb[0]
+//console.log($fx.getParams());
+
 // particles variables setup
 let features = '';
 let movers = [];
