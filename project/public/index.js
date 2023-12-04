dcc12df713d90bc9322265729bbea3a917cc2af6
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2023-12-04 15:30:26 -0500

    added new dpi settings

--- project/public/index.js
+++ project/public/index.js
@@ -1,6 +1,6 @@
 //new Q5('global');
 console.log(fxhash);
-let urlParams = new URLSearchParams(window.location.search).get('parameters');
+let urlParams = new URLSearchParams(window.location.search).get("parameters");
 urlParams = JSON.parse(urlParams);
 if (!urlParams) urlParams = {};
 
@@ -50,13 +50,12 @@ let bgSat;
 
 let animation;
 
-
 // event listeners
 
 // if d + any number is pressed, change the dpi for that number
-document.addEventListener('keydown', function(event) {
-  // Check if the pressed key is "d" and a number
-  if (event.key === 'm') {
+document.addEventListener("keydown", function (event) {
+	// Check if the pressed key is "d" and a number
+	if (event.key === "m") {
 		// toggle margin on or off
 		if (MARGIN === 0) {
 			MARGIN = oldMARGIN;
@@ -64,13 +63,13 @@ document.addEventListener('keydown', function(event) {
 			MARGIN = 0;
 		}
 		initSketch();
-  }
-	if (event.key === 'r') {
+	}
+	if (event.key === "r") {
 		// change the ratio
 		if (RATIO === 3) {
 			RATIO = 3.88;
 		} else if (RATIO === 3.88) {
-			RATIO = 1
+			RATIO = 1;
 			MARGIN = 300;
 		} else if (RATIO === 1) {
 			RATIO = 1.41;
@@ -85,7 +84,7 @@ document.addEventListener('keydown', function(event) {
 		initSketch();
 	}
 
-	if(event.key === 'p') {
+	if (event.key === "p") {
 		// change the particle number
 		if (particleNum === 400000) {
 			particleNum = 800000;
@@ -98,7 +97,7 @@ document.addEventListener('keydown', function(event) {
 		initSketch();
 	}
 
-	if(event.key === 'f'){
+	if (event.key === "f") {
 		// change the frame number
 		if (maxFrames === 10) {
 			maxFrames = 20;
@@ -111,20 +110,22 @@ document.addEventListener('keydown', function(event) {
 		initSketch();
 	}
 
-	if(event.key === 'd'){
+	if (event.key === "d") {
 		// change the dpi
 		if (dpi_val === 1) {
 			dpi_val = 2;
 		} else if (dpi_val === 2) {
 			dpi_val = 3;
 		} else if (dpi_val === 3) {
+			dpi_val = 4;
+		} else if (dpi_val === 4) {
+			dpi_val = 5;
+		} else if (dpi_val === 5) {
 			dpi_val = 1;
 		}
 
 		initSketch();
 	}
-
-
 });
 
 function setup() {
@@ -135,19 +136,19 @@ function setup() {
 }
 
 function initSketch() {
-	console.log('dpi', dpi_val);
-	console.log('ratio', RATIO);
-	console.log('margin', MARGIN);
-	console.log('particleNum', particleNum);
-	console.log('frameNum', maxFrames);
+	console.log("dpi", dpi_val);
+	console.log("ratio", RATIO);
+	console.log("margin", MARGIN);
+	console.log("particleNum", particleNum);
+	console.log("frameNum", maxFrames);
 	console.table(fxfeatures);
-	drawing=true;
+	drawing = true;
 	$fx.rand.reset();
 	fx = $fx;
 	fxhash = $fx.hash;
 	fxrand = $fx.rand;
 	rand = fxrand;
-	if(animation) clearTimeout(animation);
+	if (animation) clearTimeout(animation);
 
 	movers = [];
 
@@ -172,10 +173,14 @@ function initSketch() {
 
 	//!check if we keep complimentary colors background
 
-	bgHue = features.bgmode == 'complimentary' ? (hue + 180) % 360 : features.bgmode == 'analogous' ? (hue + 30) % 360 : hue;
-	bgSat = features.bgmode == 'transparent' ? 0 : random([2, 4, 6]);
-	bgCol = color(bgHue, bgSat, features.theme == 'bright' ? 93 : 10, 100);
-
+	bgHue =
+		features.bgmode == "complimentary"
+			? (hue + 180) % 360
+			: features.bgmode == "analogous"
+			? (hue + 30) % 360
+			: hue;
+	bgSat = features.bgmode == "transparent" ? 0 : random([2, 4, 6]);
+	bgCol = color(bgHue, bgSat, features.theme == "bright" ? 93 : 10, 100);
 
 	INIT_MOVERS();
 	let sketch = drawGenerator();
@@ -185,7 +190,6 @@ function initSketch() {
 	}
 	animate();
 }
-
 
 function* drawGenerator() {
 	let count = 0;
@@ -217,16 +221,13 @@ function* drawGenerator() {
 	}
 }
 
-
 function INIT_MOVERS() {
-
 	movers = [];
 	background(bgCol);
 	drawTexture(bgHue);
-	sclVal = features.scalevalue.split(',').map(Number);
+	sclVal = features.scalevalue.split(",").map(Number);
 	scl1 = random(sclVal[0], sclVal[1]);
 	scl2 = random(sclVal[0], sclVal[1]);
-
 
 	let ang1Max = Math.floor(map(scl1, 0.0001, 0.0008, 16000, 100, true));
 	let ang2Max = Math.floor(map(scl2, 0.0001, 0.0008, 16000, 100, true));
@@ -236,13 +237,13 @@ function INIT_MOVERS() {
 	let smallest = Math.min(ang1rnd, ang2rnd);
 	let largest = Math.max(ang1rnd, ang2rnd);
 
-	if (features.amplitudemode == 'none') {
+	if (features.amplitudemode == "none") {
 		ang1 = int(random(5));
 		ang2 = int(random(5));
-	} else if (features.amplitudemode == 'low') {
+	} else if (features.amplitudemode == "low") {
 		ang1 = smallest;
 		ang2 = smallest;
-	} else if (features.amplitudemode == 'high') {
+	} else if (features.amplitudemode == "high") {
 		ang1 = largest;
 		ang2 = largest;
 	}
@@ -264,7 +265,8 @@ function INIT_MOVERS() {
 		let y = random(yMin, yMax) * height;
 
 		let initHue = hue;
-		initHue = initHue > 360 ? initHue - 360 : initHue < 0 ? initHue + 360 : initHue;
+		initHue =
+			initHue > 360 ? initHue - 360 : initHue < 0 ? initHue + 360 : initHue;
 		movers.push(
 			new Mover(
 				x,
@@ -286,45 +288,49 @@ function INIT_MOVERS() {
 }
 
 function loadURLParams() {
-
-	window.location.search.includes('particleNum')
-	? (particleNum = parseInt(window.location.search.split('particleNum=')[1]))
-	: 800000;
-	if (window.location.search.includes('dpi')) {
-		dpi_val = parseInt(window.location.search.split('dpi=')[1]);
+	window.location.search.includes("particleNum")
+		? (particleNum = parseInt(window.location.search.split("particleNum=")[1]))
+		: 800000;
+	if (window.location.search.includes("dpi")) {
+		dpi_val = parseInt(window.location.search.split("dpi=")[1]);
 	}
-	if (window.location.search.includes('ratio=')) {
-		if (window.location.search.includes('ratio=a4') || urlParams.ratio == 'a4') {
+	if (window.location.search.includes("ratio=")) {
+		if (
+			window.location.search.includes("ratio=a4") ||
+			urlParams.ratio == "a4"
+		) {
 			RATIO = 1.41;
 			MARGIN = 250;
-		} else if (window.location.search.includes('ratio=skate') || urlParams.ratio == 'skate') {
+		} else if (
+			window.location.search.includes("ratio=skate") ||
+			urlParams.ratio == "skate"
+		) {
 			RATIO = 3.888;
 			MARGIN = 0;
 		} else if (
-			window.location.search.includes('ratio=square') ||
-			window.location.search.includes('ratio=1') ||
-			urlParams.ratio == 'square' ||
-			urlParams.ratio == '1'
+			window.location.search.includes("ratio=square") ||
+			window.location.search.includes("ratio=1") ||
+			urlParams.ratio == "square" ||
+			urlParams.ratio == "1"
 		) {
 			RATIO = 1;
 			MARGIN = 300;
 		} else if (
-			window.location.search.includes('ratio=3') ||
-			window.location.search.includes('ratio=bookmark') ||
-			urlParams.ratio == 'bookmark'
+			window.location.search.includes("ratio=3") ||
+			window.location.search.includes("ratio=bookmark") ||
+			urlParams.ratio == "bookmark"
 		) {
 			RATIO = 3;
 			MARGIN = 200;
 		} else {
-			RATIO = parseInt(window.location.search.split('ratio=')[1]);
+			RATIO = parseInt(window.location.search.split("ratio=")[1]);
 			MARGIN = 200;
 		}
 	}
 
-	if (window.location.search.includes('margin')) {
-		MARGIN = parseInt(window.location.search.split('margin=')[1]);
+	if (window.location.search.includes("margin")) {
+		MARGIN = parseInt(window.location.search.split("margin=")[1]);
 	}
-
 }
 
 function drawTexture(hue) {
@@ -335,8 +341,13 @@ function drawTexture(hue) {
 		let y = fxrand() * height;
 		let sw = 0.45 * MULTIPLIER;
 		let h = hue + fxrand() * 2 - 1;
-		let s = features.colormode != 'monochrome' ? [0, 20, 40, 60, 80, 100][parseInt(fxrand() * 6)] : 0;
-		let b = [0, 10, 10, 20, 20, 40, 60, 70, 90, 90, 100][parseInt(fxrand() * 11)];
+		let s =
+			features.colormode != "monochrome"
+				? [0, 20, 40, 60, 80, 100][parseInt(fxrand() * 6)]
+				: 0;
+		let b = [0, 10, 10, 20, 20, 40, 60, 70, 90, 90, 100][
+			parseInt(fxrand() * 11)
+		];
 		drawingContext.fillStyle = `hsla(${h}, ${s}%, ${b}%, 100%)`;
 		drawingContext.fillRect(x, y, sw, sw);
 	}
@@ -347,40 +358,62 @@ function showLoadingBar(elapsedTime, maxFrames, xMin, xMax, yMin, yMax) {
 	if (percent > 100) percent = 100;
 
 	// put the percent in the title of the page
-	document.title = percent.toFixed(0) + '%' + ' (mode ' + renderMode + ')';
+	document.title = percent.toFixed(0) + "%" + " (mode " + renderMode + ")";
 }
 
 class Mover {
-	constructor(x, y, hue, scl1, scl2, ang1, ang2, xMin, xMax, yMin, yMax, xRandDivider, yRandDivider, bgColArr) {
+	constructor(
+		x,
+		y,
+		hue,
+		scl1,
+		scl2,
+		ang1,
+		ang2,
+		xMin,
+		xMax,
+		yMin,
+		yMax,
+		xRandDivider,
+		yRandDivider,
+		bgColArr
+	) {
 		this.x = x;
 		this.y = y;
 		this.initHue = hue;
 		this.initSat =
-			features.vibrancymode === 'high'
+			features.vibrancymode === "high"
 				? [60, 70, 80, 80, 90, 100][Math.floor(fxrand() * 6)]
-				: features.vibrancymode === 'low'
+				: features.vibrancymode === "low"
 				? [0, 10, 20, 20, 30, 40][Math.floor(fxrand() * 6)]
-				: [0, 10, 20, 30, 40, 40, 60, 80, 80, 90, 100][Math.floor(fxrand() * 11)];
+				: [0, 10, 20, 30, 40, 40, 60, 80, 80, 90, 100][
+						Math.floor(fxrand() * 11)
+				  ];
 
 		this.initBri =
-			features.theme === 'bright' && features.colormode !== 'monochrome'
-				? [0, 10, 20, 20, 40, 40, 60, 70, 80, 90, 100][Math.floor(fxrand() * 11)]
-				: features.theme === 'bright' && features.colormode === 'monochrome'
+			features.theme === "bright" && features.colormode !== "monochrome"
+				? [0, 10, 20, 20, 40, 40, 60, 70, 80, 90, 100][
+						Math.floor(fxrand() * 11)
+				  ]
+				: features.theme === "bright" && features.colormode === "monochrome"
 				? [0, 0, 10, 20, 20, 30, 40, 60, 80][Math.floor(fxrand() * 9)]
 				: [40, 40, 60, 70, 70, 80, 80, 90, 100][Math.floor(fxrand() * 9)];
 		this.initAlpha = 50;
 		this.initS = 1 * MULTIPLIER;
 		this.hue = this.initHue;
-		this.sat = features.colormode === 'monochrome' || features.colormode === 'duotone' ? 0 : this.initSat;
+		this.sat =
+			features.colormode === "monochrome" || features.colormode === "duotone"
+				? 0
+				: this.initSat;
 		this.bri = this.initBri;
 		this.a = this.initAlpha;
 		this.hueStep =
-			features.colormode === 'monochrome' || features.colormode === 'fixed'
+			features.colormode === "monochrome" || features.colormode === "fixed"
 				? 1
-				: features.colormode === 'dynamic' || features.colormode === 'duotone'
+				: features.colormode === "dynamic" || features.colormode === "duotone"
 				? 10
 				: 20;
-		this.satStep = features.colorMode === 'duotone' ? 0.1 : 1;
+		this.satStep = features.colorMode === "duotone" ? 0.1 : 1;
 		this.briStep = 1;
 		this.s = this.initS;
 		this.scl1 = scl1;
@@ -402,14 +435,14 @@ class Mover {
 		this.centerY = height / 2;
 		this.borderX = width / 2;
 		this.borderY = height / 2.75;
-		this.clampvaluearray = features.clampvalue.split(',').map(Number);
-		this.uvalueArr = features.behaviorvalue.split(',').map(Number);
+		this.clampvaluearray = features.clampvalue.split(",").map(Number);
+		this.uvalueArr = features.behaviorvalue.split(",").map(Number);
 		this.uvalue = Math.min(...this.uvalueArr);
 		this.bgCol = bgColArr;
 		this.zombie = false;
-		this.zombieAlpha = features.jdlmode === 'true' ? this.initAlpha : 0;
+		this.zombieAlpha = features.jdlmode === "true" ? this.initAlpha : 0;
 		this.lineWeight =
-			typeof features.lineModeValue === 'string'
+			typeof features.lineModeValue === "string"
 				? eval(features.lineModeValue) * MULTIPLIER
 				: features.lineModeValue * MULTIPLIER;
 	}
@@ -432,22 +465,52 @@ class Mover {
 			this.uvalueArr
 		);
 
-		this.xRandSkipper = fxrand() * (this.xRandSkipperVal * MULTIPLIER * 2) - this.xRandSkipperVal * MULTIPLIER;
-		this.yRandSkipper = fxrand() * (this.xRandSkipperVal * MULTIPLIER * 2) - this.xRandSkipperVal * MULTIPLIER;
+		this.xRandSkipper =
+			fxrand() * (this.xRandSkipperVal * MULTIPLIER * 2) -
+			this.xRandSkipperVal * MULTIPLIER;
+		this.yRandSkipper =
+			fxrand() * (this.xRandSkipperVal * MULTIPLIER * 2) -
+			this.xRandSkipperVal * MULTIPLIER;
 
 		this.x += (p.x * MULTIPLIER) / this.xRandDivider + this.xRandSkipper;
 		this.y += (p.y * MULTIPLIER) / this.yRandDivider + this.yRandSkipper;
 
 		let pxy = p.x - p.y;
-		this.hue += mapValue(pxy, -this.uvalue * 2, this.uvalue * 2, -this.hueStep, this.hueStep, true);
+		this.hue += mapValue(
+			pxy,
+			-this.uvalue * 2,
+			this.uvalue * 2,
+			-this.hueStep,
+			this.hueStep,
+			true
+		);
 		this.hue = this.hue > 360 ? 0 : this.hue < 0 ? 360 : this.hue;
-		this.bri += mapValue(p.y, -this.uvalue * 2, this.uvalue * 2, -this.briStep, this.briStep, true);
+		this.bri += mapValue(
+			p.y,
+			-this.uvalue * 2,
+			this.uvalue * 2,
+			-this.briStep,
+			this.briStep,
+			true
+		);
 		this.bri = this.bri > 100 ? 0 : this.bri < 0 ? 100 : this.bri;
-		this.sat += mapValue(p.x, -this.uvalue * 2, this.uvalue * 2, -this.satStep, this.satStep, true);
-		if (features.colormode != 'monochrome' && features.colormode != 'duotone') {
+		this.sat += mapValue(
+			p.x,
+			-this.uvalue * 2,
+			this.uvalue * 2,
+			-this.satStep,
+			this.satStep,
+			true
+		);
+		if (features.colormode != "monochrome" && features.colormode != "duotone") {
 			this.sat = this.sat > 100 ? 0 : this.sat < 0 ? 100 : this.sat;
-		} else if (features.colormode === 'duotone') {
-			this.sat = this.sat > this.initSat * 1.5 ? 0 : this.sat < 0 ? this.initSat * 1.5 : this.sat;
+		} else if (features.colormode === "duotone") {
+			this.sat =
+				this.sat > this.initSat * 1.5
+					? 0
+					: this.sat < 0
+					? this.initSat * 1.5
+					: this.sat;
 		}
 
 		if (
@@ -462,27 +525,35 @@ class Mover {
 			this.a = this.zombie ? this.zombieAlpha : this.initAlpha;
 		}
 
-
-		if (this.x < (this.xMin * width)-this.lineWeight) {
-			this.x = (this.xMax * width) + (fxrand() * this.lineWeight);
+		if (this.x < this.xMin * width - this.lineWeight) {
+			this.x = this.xMax * width + fxrand() * this.lineWeight;
 			//this.a = 100;
 		}
-		if (this.x > (this.xMax * width)+this.lineWeight) {
-			this.x = (this.xMin * width) - (fxrand() * this.lineWeight);
+		if (this.x > this.xMax * width + this.lineWeight) {
+			this.x = this.xMin * width - fxrand() * this.lineWeight;
 			//this.a = 100;
 		}
-		if (this.y < (this.yMin * height)-this.lineWeight) {
-			this.y = (this.yMax * height) + (fxrand() * this.lineWeight);
+		if (this.y < this.yMin * height - this.lineWeight) {
+			this.y = this.yMax * height + fxrand() * this.lineWeight;
 			//this.a = 100;
 		}
-		if (this.y > (this.yMax * height)+this.lineWeight) {
-			this.y = (this.yMin * height) - (fxrand() * this.lineWeight);
+		if (this.y > this.yMax * height + this.lineWeight) {
+			this.y = this.yMin * height - fxrand() * this.lineWeight;
 			//this.a = 100;
 		}
-
 	}
 }
-function superCurve(x, y, scl1, scl2, ang1, ang2, octave, clampvalueArr, uvalueArr) {
+function superCurve(
+	x,
+	y,
+	scl1,
+	scl2,
+	ang1,
+	ang2,
+	octave,
+	clampvalueArr,
+	uvalueArr
+) {
 	let nx = x,
 		ny = y,
 		a1 = ang1,
@@ -510,8 +581,22 @@ function superCurve(x, y, scl1, scl2, ang1, ang2, octave, clampvalueArr, uvalueA
 	let un = oct(nx, ny, scale1, 3, octave);
 	let vn = oct(nx, ny, scale2, 2, octave);
 
-	let u = mapValue(un, -clampvalueArr[0], clampvalueArr[1], -uvalueArr[0], uvalueArr[1], true);
-	let v = mapValue(vn, -clampvalueArr[2], clampvalueArr[3], -uvalueArr[2], uvalueArr[3], true);
+	let u = mapValue(
+		un,
+		-clampvalueArr[0],
+		clampvalueArr[1],
+		-uvalueArr[0],
+		uvalueArr[1],
+		true
+	);
+	let v = mapValue(
+		vn,
+		-clampvalueArr[2],
+		clampvalueArr[3],
+		-uvalueArr[2],
+		uvalueArr[3],
+		true
+	);
 
 	return {x: u, y: v};
 }
