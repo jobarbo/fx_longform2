b7681c4850ed23dac6c20a8ae53934dd966a1327
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2024-01-04 14:31:32 -0500

    done

--- project/public/sketch.js
+++ project/public/sketch.js
@@ -11,18 +11,19 @@ let DEFAULT_SIZE = window.innerWidth;
 let DIM;
 let MULTIPLIER;
 
+// Sketch based variables
 let img;
-let angle = 0;
-let scaleFactor = 0.05;
-let angle_speed = 30;
+let colors = [];
+let sortMode = null;
 
-let pg;
+let tileCount;
+let rectSize;
+
 function preload() {
-	img = loadImage("assets/image19.png");
+	img = loadImage("assets/image20.png");
 }
 
 function setup() {
-	console.log(features);
 	features = $fx.getFeatures();
 
 	// canvas setup
@@ -30,60 +31,78 @@ function setup() {
 	MULTIPLIER = DIM / DEFAULT_SIZE;
 	c = createCanvas(DIM, DIM * RATIO);
 	pg = createGraphics(DIM, DIM * RATIO);
-	pixelDensity(dpi(2));
-	colorMode(HSB, 360, 100, 100, 100);
+	pixelDensity(dpi(4));
+	//colorMode(HSB, 360, 100, 100, 100);
 	randomSeed(fxrand() * 10000);
 	noiseSeed(fxrand() * 10000);
-
-	angleMode(DEGREES);
-	imageMode(CENTER);
-	rectMode(CENTER);
-	pg.colorMode(HSB, 360, 100, 100, 100);
-	pg.pixelDensity(dpi(3));
-
-	pg.angleMode(DEGREES);
-	pg.imageMode(CENTER);
-	pg.rectMode(CENTER);
-	pg.pg_r_h = 0;
-	pg.pg_r_s = 0;
-	pg.pg_r_b = 100;
-	pg.alpha = 100;
-	pg.alpha_speed = 0.0001;
 
 	INIT();
 }
 
-function INIT() {}
+function INIT() {
+	background(0);
+	tileCount = floor(width / 10);
+	rectSize = width / tileCount;
+	noStroke();
 
-function draw() {
-	background(255, 0);
-	translate(width / 2, height / 2);
-	blendMode(LIGHTEST);
-	pg.alpha = 100;
-	drosteEffect(img, 0, 0, width * 1.5);
-
-	blendMode(BLEND);
+	drawPixels();
 }
 
-function drosteEffect(img, x, y, size) {
-	push();
-	//translate(x, y);
-	pg.push();
-	rotate(angle);
-	tint(255, pg.alpha);
-	image(img, 0, 0, size, size);
+function draw() {
+	noLoop();
+}
 
-	pg.alpha += pg.alpha_speed;
+function drawPixels(){
+	image(img, 0, 0,width,height);
+	img.resize(width,height);
+ 	img.loadPixels();
+	colors = [];
 
-	if (pg.alpha > 100) {
-		pg.alpha = 100;
+	for (let gridY = 0; gridY < tileCount; gridY++) {
+		for (let gridX = 0; gridX < tileCount; gridX++) {
+			let px = int(gridX * rectSize);
+			let py = int(gridY * rectSize);
+			let i = (py * img.width + px) * 4;
+			let c = color(
+				img.pixels[i],
+				img.pixels[i + 1],
+				img.pixels[i + 2],
+				img.pixels[i + 3]
+			);
+			colors.push(c);
+		}
 	}
 
-	let new_size = size * scaleFactor;
-	angle += angle_speed;
+	gd.sortColors(colors, sortMode);
 
-	if (size > 5) {
-		drosteEffect(img, x, y, new_size);
+	let i = 0;
+	for (let gridY = 0; gridY < tileCount; gridY++) {
+		for (let gridX = 0; gridX < tileCount; gridX++) {
+			let c = colors[i];
+			fill(c);
+			rect(gridX * rectSize, gridY * rectSize, rectSize+1, rectSize+1);
+			i++;
+		}
+	} 
+
+
+}
+
+function keyReleased() {
+	let prevSortMode = sortMode;
+	if (key == "1") sortMode = gd.RED;
+	if (key == "2") sortMode = gd.GREEN;
+	if (key == "3") sortMode = gd.BLUE;
+	if (key == "4") sortMode = gd.ALPHA;
+	if (key == "5") sortMode = null;
+	if (key == "6") sortMode = gd.HUE;
+	if (key == "7") sortMode = gd.SATURATION;
+	if (key == "8") sortMode = gd.BRIGHTNESS;
+	if (key == "9") sortMode = gd.GRAYSCALE;
+
+	// if sortMode is changed, redraw pixels
+	if (prevSortMode != sortMode) {
+		drawPixels();
 	}
-	pop();
+	
 }
