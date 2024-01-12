bca7098ffa51618dae2e36e260fa379c879b0f89
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2024-01-12 09:36:02 -0500

    annie albers

--- project/public/sketch.js
+++ project/public/sketch.js
@@ -16,6 +16,7 @@ let cols = 4;
 let rows = 4;
 let cellSize = 350;
 let angleArray = [0,90,180,270];
+let configNum = 10;
 let angle = 0;
 let margin = 0;
 
@@ -28,7 +29,7 @@ function setup() {
 	MULTIPLIER = DIM / DEFAULT_SIZE;
 	c = createCanvas(DIM, DIM * RATIO);
 	//imageMode(CENTER)
-	pixelDensity(dpi(2));
+	pixelDensity(dpi(6));
 	colorMode(HSB, 360, 100, 100, 100);
 	randomSeed(fxrand() * 10000);
 	noiseSeed(fxrand() * 10000);
@@ -43,7 +44,7 @@ function setup() {
 function INIT() {
 
 	background(40, 10, 100);
-	margin = width / 20;
+	margin = 0;
 	cellSize = (width - margin * 2) / 14;
 	cols = int((width-margin) / cellSize);
 	rows = int((height-margin) / cellSize);
@@ -53,39 +54,31 @@ function INIT() {
 
 	
 	let index = 0;
+	let bgHue = random(0, 360);
 	for (let i = 0; i < cols; i++) {
 		for (let j = 0; j < rows; j++) {
 		let angle_index = int(random(angleArray.length));
-		console.log(angle_index);
-		let x = (width - cols * cellSize) / 2 + i * cellSize + random(-cellSize/6, cellSize/6);
-		let y = (height - rows * cellSize) / 2 + j * cellSize + random(-cellSize/6, cellSize/6);
+
+		let x = (width - cols * cellSize) / 2 + i * cellSize ;
+		let y = (height - rows * cellSize) / 2 + j * cellSize ;
 
 		graphicsArray[index] = createGraphics(cellSize, cellSize);
 		graphicsArray[index].angleMode(DEGREES);
 		graphicsArray[index].displayDensity(2);
 		graphicsArray[index].colorMode(HSB, 360, 100, 100, 100);
-		graphicsArray[index].background(random(0, 360), 30, 95, 0);
-		for (let k = 0; k < 60000; k++) {
-			let x = random(graphicsArray[index].width);
-			let y = random(graphicsArray[index].height);
-			let w = 0.5;
-			let h = 0.5;
-			graphicsArray[index].noStroke();
-			graphicsArray[index].fill(210, 50,30,50);
-			graphicsArray[index].ellipse(x, y, w, h);
-		}
+	
+		graphicsArray[index].background(bgHue, 30, 85, 100);
+
+
 		// Apply rotation directly to the graphics object before drawing the 'A'
 		graphicsArray[index].push();
 		graphicsArray[index].translate(cellSize / 2, cellSize / 2);
 		graphicsArray[index].rotate(angleArray[angle_index]);
-		graphicsArray[index].fill(210, 50,50,70);
+		// make the fill always complemantary to the background
+		graphicsArray[index].fill((bgHue + 180) % 360, 30, 35, 100);
 		graphicsArray[index].noStroke();
-		graphicsArray[index].textSize(graphicsArray[index].width*10);
-		graphicsArray[index].textAlign(CENTER, CENTER);
-		// create textured background in graphics object
-
-		//graphicsArray[index].text(letter, 0, 0);
-		graphicsArray[index].pop();
+		// draw a triangle with a point on the bottom left corner, the top right corner, and the top left corner
+		graphicsArray[index].triangle(-cellSize / 2, cellSize / 2, cellSize / 2, -cellSize / 2, -cellSize / 2, -cellSize / 2);
 
 		image(graphicsArray[index], x, y, cellSize+1, cellSize+1);
 		stroke(0);
