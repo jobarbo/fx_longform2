452a130064b68aa91e06c8d4d20311410018e354
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2024-01-26 18:33:25 -0500

    Genuary 23 done

--- project/public/sketch.js
+++ project/public/sketch.js
@@ -10,9 +10,19 @@ let CM = 1;
 let DEFAULT_SIZE = window.innerWidth;
 let DIM;
 let MULTIPLIER;
+let MARGIN = 0;
 
-let v_p;
-let v_p_pos = {x: 0, y: 0};
+let xoff;
+let yoff;
+let cellSize;
+
+let noiseLevel = 255;
+let noiseScale = 0.01;
+
+let bg_img;
+function preload() {
+	bg_img = loadImage("./assets/art.png");
+}
 
 function setup() {
 	features = $fx.getFeatures();
@@ -22,12 +32,40 @@ function setup() {
 	MULTIPLIER = DIM / DEFAULT_SIZE;
 	c = createCanvas(DIM, DIM * RATIO);
 	pixelDensity(dpi(2));
-	colorMode(HSB, 360, 100, 100, 100);
+	//colorMode(HSB, 360, 100, 100, 100);
 	randomSeed(fxrand() * 10000);
 	noiseSeed(fxrand() * 10000);
 	rectMode(CENTER);
 	angleMode(DEGREES);
-	background(50, 10, 10);
+	background(10);
+
+	xoff = random(10000000);
+	yoff = random(10000000);
+
+	cellSize = (width - MARGIN * 2) / 128;
+	bg_img.resize(width, height);
+
+	//blendMode(LIGHTEST);
+	image(bg_img, 0, 0);
+	createGrid();
+}
+
+function createGrid() {
+	for (x = MARGIN; x < width - MARGIN; x += cellSize) {
+		for (y = MARGIN; y < height - MARGIN; y += cellSize) {
+			/* 			let n_hx = noiseScale * x;
+			let n_hy = noiseScale * y;
+			let hue = noise(n_hx, n_hy) * 360; */
+
+			let pix = bg_img.get(x, y);
+
+			fill(pix);
+			strokeWeight(0);
+			rect(x + cellSize / 2, y + cellSize / 2, cellSize, cellSize);
+			yoff += 0.001;
+		}
+		xoff += 0.0000001;
+	}
 }
 
 function draw() {
