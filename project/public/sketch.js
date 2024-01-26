8b0701d75d8cc939734814920ccef4e927bfb37c
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2024-01-26 01:00:07 -0500

    done

--- project/public/sketch.js
+++ project/public/sketch.js
@@ -20,20 +20,32 @@ function setup() {
 	// canvas setup
 	DIM = min(windowWidth * CM, windowHeight * CM);
 	MULTIPLIER = DIM / DEFAULT_SIZE;
-	c = createCanvas(DIM, DIM * RATIO);
+	c = createCanvas(DIM, DIM * RATIO, WEBGL);
 	pixelDensity(dpi(2));
 	colorMode(HSB, 360, 100, 100, 100);
 	randomSeed(fxrand() * 10000);
 	noiseSeed(fxrand() * 10000);
-	rectMode(CENTER);
+	//rectMode(CENTER);
 	angleMode(DEGREES);
-	background(220, 80, 20);
+	background(50, 40, 100);
+	brush.scaleBrushes(1);
+	brush.field("seabed");
+	translate(-width / 2, -height / 2);
+	let available_brushes = brush.box();
+	console.log(available_brushes);
+	brush.set("rotring", "#002185", 11);
+	brush.noStroke();
+	brush.fill("#002185", 255);
+	brush.circle(width / 4, height / 2, 140, true);
+	brush.field("seabed");
 
-	v_p = new VanishingPoint(width / 2, height / 2, 1, 3, true);
-	console.log(v_p);
-	console.log(width, height);
+	brush.noHatch();
+
+	brush.rect(width / 2, height / 2, 2, 550, CENTER);
+
+	brush.noHatch();
+
+	brush.rect(width / 1.25, height / 2, 300, 750, CENTER);
 }
 
-function draw() {
-	noLoop();
-}
+function draw() {}
