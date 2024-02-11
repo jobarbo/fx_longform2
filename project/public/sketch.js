ea6c2d749ac84fbfdbe294a011dba5a637eb6905
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2024-02-11 00:47:56 -0500

    progress

--- project/public/sketch.js
+++ project/public/sketch.js
@@ -7,9 +7,10 @@ let W = window.innerWidth;
 let H = window.innerHeight;
 
 let CM = 1;
-let DEFAULT_SIZE = 600;
+let DEFAULT_SIZE = 400;
 let DIM;
 let MULTIPLIER;
+let MAX_FRAMES = 350;
 
 let particle_num = 5000;
 
@@ -20,7 +21,9 @@ let woff = 0.3;
 let xi = Math.random * 1000000000000;
 let yi = Math.random * 1000000000000;
 
-let n_range;
+let pos_range;
+let n_range_min = 0.1;
+let n_range_max = 0.9;
 
 function setup() {
 	features = $fx.getFeatures();
@@ -29,7 +32,7 @@ function setup() {
 	DIM = min(windowWidth * CM, windowHeight * CM);
 	MULTIPLIER = DIM / DEFAULT_SIZE;
 	c = createCanvas(DIM, DIM * RATIO);
-	pixelDensity(dpi(3));
+	pixelDensity(dpi(maxDPI));
 	colorMode(HSB, 360, 100, 100, 100);
 	randomSeed(fxrand() * 10000);
 	noiseSeed(fxrand() * 10000);
@@ -40,7 +43,7 @@ function setup() {
 	yi = random(1000000000000);
 	/* 	xi = 0; */
 	/* 	yi = 0; */
-	n_range = width / 2.55;
+	pos_range = width;
 }
 
 function draw() {
@@ -50,9 +53,20 @@ function draw() {
 	//let angle = int(random([0, 45, 90]));
 	//let angle = int(random([0, 45, 90, 180, 225, 270]));
 	//let angle = int(random([0, 45, 90, 135, 180, 225, 270, 315]));
+	//let angle = random([45, 225]);
+	//let angle = random([45, 135, 225, 315]);
+	//let angle = random([0, 180]);
+	//let angle = random([90, 270]);
 	let angle = 45;
+	//let angle = random([0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300, 310, 320, 330, 340, 350]);
 	rotate(angle);
+	scale(0.5);
 	paint(xoff, yoff, woff, particle_num, xi, yi);
+
+	if (frameCount >= MAX_FRAMES) {
+		document.complete = true;
+		noLoop();
+	}
 }
 
 function paint(xoff, yoff, woff, particle_num, xi, yi) {
@@ -60,53 +74,60 @@ function paint(xoff, yoff, woff, particle_num, xi, yi) {
 		xoff = random(0, 1);
 		yoff = random(0, 1);
 		woff = random(0, 1);
-		//noiseDetail(62, 0.45);
+		//noiseDetail(10, 0.5);
 		//! Simple Block
-		/* 		let x = map(noise(xoff), 0.1, 0.9, -n_range, n_range, true);
-		let y = map(noise(yoff), 0.1, 0.9, -n_range, n_range, true); */
+		/* 		let x = map(noise(xoff),n_range_min, n_range_max, -pos_range, pos_range, true);
+		let y = map(noise(yoff),n_range_min, n_range_max, -pos_range, pos_range, true); */
 		//! Electron microscope
-		/* let x = map(noise(xoff, yoff), 0.1, 0.9, -n_range, n_range, true);
-		let y = map(noise(yoff, xoff), 0.1, 0.9, -n_range, n_range, true); */
+		/* 		let x = map(noise(xoff, yoff), n_range_min, n_range_max, -pos_range, pos_range, true);
+		let y = map(noise(yoff, xoff), n_range_min, n_range_max, -pos_range, pos_range, true); */
 		//!block Rect
-		/* 		let x = map(noise(xoff, xoff, xi), 0.1, 0.9, -n_range, n_range, true);
-		let y = map(noise(yoff, yoff, yi), 0.1, 0.9, -n_range, n_range, true); */
+		/* 		let x = map(noise(xoff, xoff, xi),n_range_min, n_range_max, -pos_range, pos_range, true);
+		let y = map(noise(yoff, yoff, yi),n_range_min, n_range_max, -pos_range, pos_range, true); */
 
 		//!Drapery Yin Yang
-		/* 		let x = map(noise(xoff, yoff, xi), 0.1, 0.9, -n_range, n_range, true);
-		let y = map(noise(xoff, yoff, yi), 0.1, 0.9, -n_range, n_range, true); */
-
+		/* let x = map(noise(xoff, yoff, xi), n_range_min, n_range_max, -pos_range, pos_range, true);
+		let y = map(noise(xoff, yoff, yi), n_range_min, n_range_max, -pos_range, pos_range, true);
+ */
 		//!Drapery Equilibrium
-		/* let x = map(noise(xoff, yoff, xi), 0.1, 0.9, -n_range, n_range, true);
-		let y = map(noise(yoff, xoff, yi), 0.1, 0.9, -n_range, n_range, true); */
+		/* 		let x = map(noise(xoff, yoff, xi), n_range_min, n_range_max, -pos_range, pos_range, true);
+		let y = map(noise(yoff, xoff, yi), n_range_min, n_range_max, -pos_range, pos_range, true); */
 
 		//! Astral Beings
-		/* 		let x = map(noise(xoff, random([xoff, yoff, xi])), 0.1, 0.9, -n_range, n_range, true);
-		let y = map(noise(yoff, random([yoff, xoff, xi])), 0.1, 0.9, -n_range, n_range, true); */
+		let x = map(noise(xoff, random([xoff, yoff, yi])), n_range_min, n_range_max, -pos_range, pos_range, true);
+		let y = map(noise(yoff, random([yoff, xoff, yi])), n_range_min, n_range_max, -pos_range, pos_range, true);
 
 		//! Astral Beings 2
-		/* let x = map(noise(xoff, random([yoff, yoff, xi])), 0.1, 0.9, -n_range, n_range, true);
-		let y = map(noise(yoff, random([xoff, xoff, xi])), 0.1, 0.9, -n_range, n_range, true); */
+		/* 		let x = map(noise(xoff, random([yoff, yoff, yi])), n_range_min, n_range_max, -pos_range, pos_range, true);
+		let y = map(noise(yoff, random([xoff, xoff, yi])), n_range_min, n_range_max, -pos_range, pos_range, true); */
+
+		//! Astral Beings 3
+		/* 	let x = map(noise(xoff, xoff, random([yoff, yoff, yi])), n_range_min, n_range_max, -pos_range, pos_range, true);
+		let y = map(noise(yoff, yoff, random([xoff, xoff, yi])), n_range_min, n_range_max, -pos_range, pos_range, true); */
+
+		/* 		let x = map(noise(yoff, xoff, random([xoff, yoff, yi])), n_range_min, n_range_max, -pos_range, pos_range, true);
+		let y = map(noise(xoff, yoff, random([xoff, yoff, yi])), n_range_min, n_range_max, -pos_range, pos_range, true); */
 
 		//! Astral Beings Asymmetrical
-		let x = map(noise(xoff, random([xoff, yoff, xi])), 0.1, 0.9, -n_range, n_range, true);
-		let y = map(noise(yoff, random([yoff, xoff, yi])), 0.1, 0.9, -n_range, n_range, true);
+		/* let x = map(noise(xoff, random([xoff, yoff, xi])), n_range_min, n_range_max, -pos_range, pos_range, true);
+		let y = map(noise(yoff, random([yoff, xoff, yi])), n_range_min, n_range_max, -pos_range, pos_range, true); */
 
 		//! Hybrid Drapery Blocks
-		/* let x = map(noise(xoff, random([xoff, xoff, yi])), 0.1, 0.9, -n_range, n_range, true);
-		let y = map(noise(yoff, random([yoff, xoff, yi])), 0.1, 0.9, -n_range, n_range, true); */
+		/* let x = map(noise(xoff, random([xoff, xoff, yi])), n_range_min, n_range_max, -pos_range, pos_range, true);
+		let y = map(noise(yoff, random([yoff, xoff, yi])), n_range_min, n_range_max, -pos_range, pos_range, true); */
 
 		//!complex organism (aliens)
-		/* 		let x = map(noise(xoff, yoff, random([yoff, xoff])), 0.1, 0.9, -n_range, n_range, true);
-		let y = map(noise(yoff, xoff, random([yoff, xoff])), 0.1, 0.9, -n_range, n_range, true); */
+		/* 		let x = map(noise(xoff, yoff, random([yoff, xoff, yi])), n_range_min, n_range_max, -pos_range, pos_range, true);
+		let y = map(noise(yoff, xoff, random([yoff, xoff, yi])), n_range_min, n_range_max, -pos_range, pos_range, true); */
 
-		let elW = 0.25 * MULTIPLIER;
+		let elW = 0.2 * MULTIPLIER;
 
 		noStroke();
 		//stroke(190, 53, 89, 0);
 		fill(0, 75, 10, 100);
 		ellipse(x, y, elW, elW);
 
-		xi += 0.00000000000000001;
-		yi += 0.00000000000000001;
+		xi += 0.0000000000000000000001;
+		yi += 0.0000000000000000000001;
 	}
 }
