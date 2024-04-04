0a7790741fbcf3f0999eaf4a30e9c0cd71b48d6d
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2024-04-04 16:27:44 -0400

    Major change: added loading in title, generator function and tested textures

--- project/public/sketch.js
+++ project/public/sketch.js
@@ -43,19 +43,24 @@ let base_angle = 0;
 // let angle1 = [0, 90, 180, 270];
 // let angle1 = [90, 270];
 let angle1 = [45];
-// let angle1 = [0, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300, 320, 340];
+//let angle1 = [0, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300, 320, 340];
 // let angle1 = [85, 105, 125, 145, 305, 325, 345, 5]; //! y-axis asymmetry
 //let angle1 = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300, 310, 320, 330, 340, 350];
 let angle2 = 0;
 
-let particle_num = Math.floor(10000 / angle1.length);
-
-let angle_index = 0;
+let animation;
+let drawing = true;
+let elapsedTime = 0;
+let renderStart = Date.now();
+let framesRendered = 0;
+let totalElapsedTime = 0;
 
 let MAX_FRAMES = Math.floor(mapValue(angle1.length, 1, 36, 700, 1400));
+let particle_num = Math.floor(10000 / angle1.length);
 
-console.log(MAX_FRAMES);
-console.log(particle_num);
+let cycle = mapValue(angle1.length, 1, 36, 1, 20);
+
+console.log(cycle);
 
 let nx_scale = 0.00005;
 let ny_scale = 0.00005;
@@ -63,29 +68,30 @@ let ny_scale = 0.00005;
 let ny_scale = 0.001; */
 
 let skipperMax = 0;
-
 let xRandSkipperVal;
 let yRandSkipperVal;
-
 let xRandSkipper = 0;
 let yRandSkipper = 0;
-
 let apertureLow = 0.01;
 let apertureHigh = 0.01;
-
-let yoff_l_init = 1;
-let xoff_l_init = 1;
-
+let yoff_l_init = 1.1;
+let xoff_l_init = 1.1;
 let xoff_l = xoff_l_init;
 let xoff_h = 2;
-
 let yoff_l = yoff_l_init;
 let yoff_h = 2;
-
 let cos_val;
+let angle_index = 0;
 
 function setup() {
 	features = $fx.getFeatures();
+
+	elapsedTime = 0;
+	framesRendered = 0;
+	drawing = true;
+	startTime = frameCount;
+	renderStart = Date.now();
+
 	C_WIDTH = min(DEFAULT_SIZE * CM, DEFAULT_SIZE * CM);
 	MULTIPLIER = C_WIDTH / DEFAULT_SIZE;
 	c = createCanvas(C_WIDTH, C_WIDTH * RATIO);
@@ -95,7 +101,7 @@ function setup() {
 	noiseSeed(fxrand() * 10000);
 	rectMode(CENTER);
 	angleMode(DEGREES);
-	//background(45, 0, 4);
+	//background(45, 0, 5);
 	//background(45, 10, 100);
 	drawingContext.globalCompositeOperation = "source-over";
 	let gradient = drawingContext.createRadialGradient(width / 2, height / 2, 0, width / 2, height / 2, width / 2);
@@ -110,44 +116,73 @@ function setup() {
 
 	xRandSkipperVal = random([0.01, random([0.1, 1, 2, 5, 10, 25, 50, 100])]);
 	yRandSkipperVal = xRandSkipperVal;
+
+	let sketch = drawGenerator();
+	function animate() {
+		animation = setTimeout(animate, 0);
+		sketch.next();
+	}
+	animate();
 }
 
-function draw() {
-	// Draw with p5.js things
-	//blendMode(SCREEN);
-	cos_val = sin(frameCount * 50);
-	//displacement2 = random([100, 200, 300]);
+function* drawGenerator() {
+	let count = 0;
+	let generator_frameCount = 0;
+	let draw_every = cycle;
 	translate(width / 2, height / 2);
 
-	let scale1 = 1;
-	//let scale2 = 1;
-	xoff_l = map(cos_val, -1, -0.25, 1.99, 1, true);
-	yoff_l = map(cos_val, 0.25, 1, 1, 1.99, true);
-	for (let i = 0; i < angle1.length; i++) {
-		push();
-		rotate(angle1[i]);
-		scale(scale1);
-		paint(xoff_l, xoff_h, yoff_l, yoff_h, particle_num, xi, yi, scale1);
+	while (true) {
+		// Draw with p5.js things
+		//blendMode(SCREEN);
+		cos_val = sin(generator_frameCount * 50);
+		//displacement2 = random([100, 200, 300]);
+		let scale1 = 1;
+		//let scale2 = 1;
+		xoff_l = map(cos_val, -1, 0, 1.99, 1.2, true);
+		yoff_l = map(cos_val, -0, 1, 1.2, 1.99, true);
+		for (let i = 0; i < angle1.length; i++) {
+			push();
+			rotate(angle1[i]);
+			scale(scale1);
+			paint(xoff_l, xoff_h, yoff_l, yoff_h, particle_num, xi, yi, scale1, cos_val);
 
-		/* 	paint(random([0.15, 0.5, 0.75]), random([0.51, 1, 1.25]), random([0.15, 0.5, 0.75]), random([0.51, 1, 1.25]), particle_num, xi, yi, scale1); */
-		pop();
-		/* 	push();
-		rotate(angle2);
-		translate(0, displacement2);
-		scale(scale2);
-		paint(0.4, 0.6, 0.1, 0.9, particle_num, xi, yi, scale2);
-		pop(); */
-	}
-	blendMode(BLEND);
+			/* 	paint(random([0.15, 0.5, 0.75]), random([0.51, 1, 1.25]), random([0.15, 0.5, 0.75]), random([0.51, 1, 1.25]), particle_num, xi, yi, scale1); */
+			pop();
+			/* 	push();
+			rotate(angle2);
+			translate(0, displacement2);
+			scale(scale2);
+			paint(0.4, 0.6, 0.1, 0.9, particle_num, xi, yi, scale2);
+			pop(); */
 
-	if (frameCount >= MAX_FRAMES) {
-		document.complete = true;
-		noLoop();
-		console.log("done");
+			if (count >= draw_every) {
+				count = 0;
+				yield;
+			}
+			count++;
+		}
+
+		elapsedTime = generator_frameCount - startTime;
+
+		showLoadingBar(elapsedTime, MAX_FRAMES, renderStart);
+
+		generator_frameCount++;
+		if (elapsedTime > MAX_FRAMES && drawing) {
+			window.rendered = c.canvas;
+			//hl.token.capturePreview();
+			document.complete = true;
+			// calculate the time it took to render the image
+			let endTime = Date.now();
+			let timeDiff = endTime - renderStart;
+			console.log("Render time: " + timeDiff + " ms");
+
+			noLoop();
+			return;
+		}
 	}
 }
 
-function paint(xoff_l, xoff_h, yoff_l, yoff_h, particle_num, xi, yi, scale) {
+function paint(xoff_l, xoff_h, yoff_l, yoff_h, particle_num, xi, yi, scale, cos_val) {
 	for (let s = 0; s < particle_num; s++) {
 		xoff = random(xoff_l, xoff_h);
 		yoff = random(yoff_l, yoff_h);
@@ -198,7 +233,7 @@ function paint(xoff_l, xoff_h, yoff_l, yoff_h, particle_num, xi, yi, scale) {
 		/* let x = map(noise(xoff, yoff, random([yoff, xoff, yi])), n_range_min, n_range_max, -pos_range, pos_range, true);
 		let y = map(noise(yoff, xoff, random([yoff, xoff, yi])), n_range_min, n_range_max, -pos_range, pos_range, true); */
 
-		//skipperMax = map(frameCount, MAX_FRAMES / 100, MAX_FRAMES / 1.5, apertureHigh, apertureLow, true);
+		//skipperMax = map(elapsedTime, MAX_FRAMES / 100, MAX_FRAMES / 1.5, apertureHigh, apertureLow, true);
 		skipperMax = map(abs(cos_val), 0, 1, apertureHigh, apertureLow, true);
 
 		xRandSkipperVal = random(0.01, [random(0.01, skipperMax)]);
@@ -207,16 +242,18 @@ function paint(xoff_l, xoff_h, yoff_l, yoff_h, particle_num, xi, yi, scale) {
 		xRandSkipper = randomGaussian(0, xRandSkipperVal);
 		yRandSkipper = randomGaussian(0, yRandSkipperVal);
 
-		//let w = map(frameCount, MAX_FRAMES / 5, MAX_FRAMES / 1.5, 0.22, 0.15, true);
+		//let w = map(elapsedTime, MAX_FRAMES / 5, MAX_FRAMES / 1.5, 0.22, 0.15, true);
 		let w = map(abs(cos_val), 0, 1, 0.01, 0.2, true);
+
 		let elW = w * MULTIPLIER;
 		let ab_x = x + xRandSkipper;
 		let ab_y = y + yRandSkipper;
 
 		hue = map(sqrt(ab_x * ab_x + ab_y * ab_y), 0, pos_range / 1.7, 310, 210, true);
-		sat = map(frameCount, 0, MAX_FRAMES / 2.5, 60, 100, true);
-		bri = map(frameCount, MAX_FRAMES / 3.5, MAX_FRAMES / 2, 100, bri_min, true);
-		bri_min = map(frameCount, MAX_FRAMES / 2, MAX_FRAMES / 1, 80, 40, true);
+		sat = map(elapsedTime, 0, MAX_FRAMES / 2.5, 60, 100, true);
+		bri = map(elapsedTime, MAX_FRAMES / 3.5, MAX_FRAMES / 2, 100, bri_min, true);
+		bri_min = map(elapsedTime, MAX_FRAMES / 2, MAX_FRAMES / 1, 80, 40, true);
+
 		noStroke();
 		//fill(0, 75, 10, 100);
 		fill(hue, sat, bri, 100);
@@ -225,4 +262,25 @@ function paint(xoff_l, xoff_h, yoff_l, yoff_h, particle_num, xi, yi, scale) {
 		xi += 0.0000000000000000000001;
 		yi += 0.0000000000000000000001;
 	}
+}
+
+function showLoadingBar(elapsedTime, MAX_FRAMES, renderStart) {
+	framesRendered++;
+	let currentTime = Date.now();
+	totalElapsedTime = currentTime - renderStart;
+
+	let percent = (elapsedTime / MAX_FRAMES) * 100;
+	if (percent > 100) percent = 100;
+
+	let averageFrameTime = totalElapsedTime / framesRendered;
+
+	let remainingFrames = MAX_FRAMES - framesRendered;
+	let estimatedTimeRemaining = averageFrameTime * remainingFrames;
+
+	// Convert milliseconds to seconds
+	let timeLeftSec = Math.round(estimatedTimeRemaining / 1000);
+
+	// put the percent in the title of the page
+	document.title = percent.toFixed(0) + "%";
+	// show a loading bar on the bottom of the canvas
 }
