4690d3c31f7b826b9cd33f055d4b34fc2e8ee9aa
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2024-04-09 12:20:28 -0400

    more natural feel

--- project/public/object/mover.js
+++ project/public/object/mover.js
@@ -3,19 +3,19 @@ class Mover {
 		this.x = x;
 		this.y = y;
 		this.initHue = hue;
-		this.initSat = random([20, 30, 40, 50, 60, 70, 80, 90, 100]);
+		this.initSat = random([50, 60, 70, 80, 90, 100]);
 		//this.initBri = random([0, 0, 10, 20]);
-		this.initBri = random([0, 10, 20, 30, 40, 50, 60, 70, 70, 70, 80, 80, 90, 100]);
+		this.initBri = random([0, 10, 20, 30, 40, 50, 60, 70, 70, 70, 80, 80, 80, 80, 90, 90, 100]);
 		this.initAlpha = 100;
-		this.initS = 0.4 * MULTIPLIER;
+		this.initS = 0.45 * MULTIPLIER;
 		this.s = this.initS;
 		this.hue = this.initHue;
 		this.sat = this.initSat;
 		this.bri = this.initBri;
 		this.a = this.initAlpha;
-		this.hueStep = 16;
-		this.satStep = 15;
-		this.briStep = 5;
+		this.hueStep = 8;
+		this.satStep = 1;
+		this.briStep = 1;
 		this.scl1Init = scl1;
 		this.scl2Init = scl2;
 		this.scl1 = scl1;
@@ -36,14 +36,13 @@ class Mover {
 		this.yMax = yMax;
 		this.xLimit = 0.00015;
 		this.yLimit = 0.00015;
-		this.oct = 14;
+		this.oct = 6;
 		this.centerX = width / 2;
 		this.centerY = height / 2;
 		this.borderX = width / 2;
 		this.borderY = height / 2.75;
-		this.uvalue = 20;
+		this.uvalue = 15;
 		this.isBordered = true;
-
 		this.ang1Zone = ang1Zone;
 		this.ang2Zone = ang2Zone;
 		this.scl1Zone = scl1Zone;
@@ -60,14 +59,13 @@ class Mover {
 		let distCircle = sdf_circle([this.x, this.y], [this.centerX, this.centerY], 302);
 
 		//! CHECK WHY ANG AND SCL IS NOT AGNOSTIC TO MULTIPLIER
-		this.ang1 = int(map(distCircle, -300, 0, 1, 8000, true));
-		this.ang2 = int(map(distCircle, -300, 0, 3000, 1, true));
-		/* 		this.scl1 = map(distCircle, -300, -2, 0.005, 0.003, true);
-		this.scl2 = 0.002; */
+		this.ang1 = int(map(distCircle, -300, -2, 700, 4000, true));
+		this.ang2 = 500;
+		this.scl1 = map(distCircle, -300, -2, 0.005, 0.003, true);
 		//! CHECK WHY ANG AND SCL IS NOT AGNOSTIC TO MULTIPLIER
-		let p = superCurve(this.x, this.y, this.scl1, this.scl2, this.ang1, this.ang2, this.oct);
-		this.xRandDivider = fxrand() * 6;
-		this.yRandDivider = fxrand() * 6;
+		let p = superCurve(this.x, this.y, this.scl1, this.scl2, this.ang1, this.ang2, this.oct, this.uvalue);
+		this.xRandDivider = fxrand() * 6 + 0.000001;
+		this.yRandDivider = fxrand() * 6 + 0.000001;
 
 		this.x += (p.x * MULTIPLIER) / this.xRandDivider + this.xRandSkipper;
 		this.y += (p.y * MULTIPLIER) / this.yRandDivider + this.yRandSkipper;
@@ -85,13 +83,13 @@ class Mover {
 		if (this.isBordered) {
 			if (distCircle > fxrand() * 8 - 4) {
 				let r = fxrand() * 2 * PI;
-				this.x = this.centerX + cos(r) * random(298, 304);
-				this.y = this.centerY + sin(r) * random(298, 304);
+				this.x = this.centerX + cos(r) * random(298, 302);
+				this.y = this.centerY + sin(r) * random(298, 302);
 			}
 		}
 	}
 }
-function superCurve(x, y, scl1, scl2, ang1, ang2, octave) {
+function superCurve(x, y, scl1, scl2, ang1, ang2, octave, uvalue) {
 	let nx = x,
 		ny = y,
 		a1 = ang1,
@@ -99,7 +97,8 @@ function superCurve(x, y, scl1, scl2, ang1, ang2, octave) {
 		scale1 = scl1,
 		scale2 = scl2,
 		dx,
-		dy;
+		dy,
+		uv = uvalue;
 
 	dx = oct(nx, ny, scale1, 0, octave);
 	dy = oct(nx, ny, scale2, 2, octave);
@@ -122,8 +121,8 @@ function superCurve(x, y, scl1, scl2, ang1, ang2, octave) {
 	/* 	let u = clamp(un + 0.5, 0, 1) * 21 - 1;
 	let v = clamp(vn + 0.5, 0, 1) * 21 - 20; */
 
-	let u = map(un, -0.5, 0.5, -20, 1, true);
-	let v = map(vn, -0.5, 0.5, -1, 20, true);
+	let u = map(un, -0.5, 0.5, -uv, 1, true);
+	let v = map(vn, -0.5, 0.5, -1, uv, true);
 
 	//let p = createVector(u, v);
 	return {x: u, y: v};
