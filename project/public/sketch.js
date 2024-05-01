6c648e24f23e98800bf5b7ccf4b568f806c12e98
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2024-05-01 16:19:17 -0400

    back to flower color

--- project/public/sketch.js
+++ project/public/sketch.js
@@ -13,7 +13,7 @@ let MULTIPLIER;
 //let MAX_FRAMES = 700;
 
 let xoff = 0.6;
-let yoff = 0.001;
+let yoff = 0.3;
 
 let xi = Math.random * 1000000000000;
 let yi = Math.random * 1000000000000;
@@ -37,13 +37,13 @@ let displacement2 = 100;
 let base_angle = 0;
 // let angle = [0, 45, 90];
 // let angle = [0, 45, 90, 180, 225, 270];
-//let angle1 = [0, 45, 90, 135, 180, 225, 270, 315];
+let angle1 = [0, 45, 90, 135, 180, 225, 270, 315];
 //let angle1 = [45, 225];
 //let angle1 = [45, 135, 225, 315];
 // let angle1 = [0, 90, 180, 270];
 // let angle1 = [90, 270];
 //let angle1 = [45];
-let angle1 = [0, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300, 320, 340];
+//let angle1 = [0, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300, 320, 340];
 // let angle1 = [85, 105, 125, 145, 305, 325, 345, 5]; //! y-axis asymmetry
 //let angle1 = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300, 310, 320, 330, 340, 350];
 let angle2 = 0;
@@ -102,14 +102,14 @@ function setup() {
 	noiseSeed(fxrand() * 10000);
 	rectMode(CENTER);
 	angleMode(DEGREES);
-	background(45, 0, 5);
+	//background(45, 0, 5);
 	//background(45, 10, 100);
-	/* 	drawingContext.globalCompositeOperation = "source-over";
+	drawingContext.globalCompositeOperation = "source-over";
 	let gradient = drawingContext.createRadialGradient(width / 2, height / 2, 0, width / 2, height / 2, width / 2);
 	gradient.addColorStop(0.5, "hsl(25, 100%, 98%)");
 	gradient.addColorStop(0.8, "hsl(36, 100%, 95%)");
 	drawingContext.fillStyle = gradient;
-	drawingContext.fillRect(0, 0, width, height); */
+	drawingContext.fillRect(0, 0, width, height);
 
 	xi = random(1000000000000);
 	yi = random(1000000000000);
@@ -134,13 +134,13 @@ function* drawGenerator() {
 
 	while (true) {
 		// Draw with p5.js things
-		blendMode(SCREEN);
-		cos_val = sin(generator_frameCount * 50);
+		//blendMode(SCREEN);
+		cos_val = cos(generator_frameCount * 50);
 		//displacement2 = random([100, 200, 300]);
 		let scale1 = 1;
 		//let scale2 = 1;
-		xoff_l = map(cos_val, -1, 0, 1.98, 1.65, true);
-		yoff_l = map(cos_val, -0, 1, 1.65, 1.98, true);
+		xoff_l = map(cos_val, -1, 0, 1.998, 1.5, true);
+		yoff_l = map(cos_val, 0, 1, 1.5, 1.998, true);
 		for (let i = 0; i < angle1.length; i++) {
 			push();
 			rotate(angle1[i]);
@@ -187,7 +187,7 @@ function paint(xoff_l, xoff_h, yoff_l, yoff_h, particle_num, xi, yi, scale, cos_
 	for (let s = 0; s < particle_num; s++) {
 		xoff = random(xoff_l, xoff_h);
 		yoff = random(yoff_l, yoff_h);
-		noiseDetail(4, 0.67);
+		//noiseDetail(4, 0.5);
 		//! Simple Block
 		/* 		let x = map(noise(xoff), n_range_min, n_range_max, -pos_range, pos_range, true);
 		let y = map(noise(yoff), n_range_min, n_range_max, -pos_range, pos_range, true); */
@@ -217,8 +217,8 @@ function paint(xoff_l, xoff_h, yoff_l, yoff_h, particle_num, xi, yi, scale, cos_
 		let y = map(noise(yoff, random([xoff, xoff, yi])), n_range_min, n_range_max, -pos_range, pos_range, true); */
 
 		//! Astral Beings 3
-		/* 		let x = map(noise(xoff, xoff, random([yoff, yoff, yi])), n_range_min, n_range_max, -pos_range, pos_range, true);
-		let y = map(noise(yoff, yoff, random([xoff, xoff, yi])), n_range_min, n_range_max, -pos_range, pos_range, true); */
+		let x = map(noise(xoff, xoff, random([yoff, yoff, yi])), n_range_min, n_range_max, -pos_range, pos_range, true);
+		let y = map(noise(yoff, yoff, random([xoff, xoff, yi])), n_range_min, n_range_max, -pos_range, pos_range, true);
 		/* 		let x = map(noise(yoff, xoff, random([xoff, yoff, yi])), n_range_min, n_range_max, -pos_range, pos_range, true);
 		let y = map(noise(xoff, yoff, random([xoff, yoff, yi])), n_range_min, n_range_max, -pos_range, pos_range, true); */
 
@@ -231,8 +231,8 @@ function paint(xoff_l, xoff_h, yoff_l, yoff_h, particle_num, xi, yi, scale, cos_
 		let y = map(noise(yoff, random([yoff, xoff, yi])), n_range_min, n_range_max, -pos_range, pos_range, true); */
 
 		//!complex organism (aliens)
-		let x = map(noise(xoff, yoff, random([xoff, xi, yi])), n_range_min, n_range_max, -pos_range, pos_range, true);
-		let y = map(noise(yoff, xoff, random([yoff, xi, yi])), n_range_min, n_range_max, -pos_range, pos_range, true);
+		/* 		let x = map(noise(xoff, yoff, random([xoff, xi, yi])), n_range_min, n_range_max, -pos_range, pos_range, true);
+		let y = map(noise(yoff, xoff, random([yoff, xi, yi])), n_range_min, n_range_max, -pos_range, pos_range, true); */
 
 		//skipperMax = map(elapsedTime, MAX_FRAMES / 100, MAX_FRAMES / 1.5, apertureHigh, apertureLow, true);
 		skipperMax = map(abs(cos_val), 0, 1, apertureHigh, apertureLow, true);
@@ -244,16 +244,16 @@ function paint(xoff_l, xoff_h, yoff_l, yoff_h, particle_num, xi, yi, scale, cos_
 		yRandSkipper = randomGaussian(0, yRandSkipperVal);
 
 		//let w = map(elapsedTime, MAX_FRAMES / 5, MAX_FRAMES / 1.5, 0.22, 0.15, true);
-		let w = map(abs(cos_val), 0, 1, 0.1, 0.2, true);
+		let w = map(abs(cos_val), 0, 1, 0.01, 0.3, true);
 
 		let elW = w * MULTIPLIER;
 		let ab_x = x + xRandSkipper;
 		let ab_y = y + yRandSkipper;
-
+		hue = map(sqrt(ab_x * ab_x + ab_y * ab_y), 0, pos_range / 1.7, 310, 210, true);
 		//hue = map(sqrt(ab_x * ab_x + ab_y * ab_y), 0, pos_range / 1.7, 40, 0, true);
-		hue = map(abs(cos_val), 0, 1, 0, 40, true);
+		hue = map(abs(cos_val), 0.4, 1, 310, 210, true);
 		sat = map(elapsedTime, 0, MAX_FRAMES / 2.5, 60, 100, true);
-		bri = map(elapsedTime, MAX_FRAMES / 3.5, MAX_FRAMES / 2, bri_min, 100, true);
+		bri = map(elapsedTime, MAX_FRAMES / 3.5, MAX_FRAMES / 2, 100, bri_min, true);
 		bri_min = map(elapsedTime, MAX_FRAMES / 2, MAX_FRAMES / 1, 80, 80, true);
 
 		noStroke();
