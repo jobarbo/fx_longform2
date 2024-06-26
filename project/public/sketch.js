2d4296cce986c4bc9e9f2710560d066ed59fb30d
Jonathan Barbeau <jonathan@unikmedia.ca>
2024-06-26 11:04:27 -0400

    testing svg

--- project/public/sketch.js
+++ project/public/sketch.js
@@ -16,20 +16,7 @@ let v_p_pos = {x: 0, y: 0};
 
 function setup() {
 	features = $fx.getFeatures();
-
-	// canvas setup
-	DIM = min(windowWidth * CM, windowHeight * CM);
-	MULTIPLIER = DIM / DEFAULT_SIZE;
-	c = createCanvas(DIM, DIM * RATIO);
-	pixelDensity(dpi(2));
-	colorMode(HSB, 360, 100, 100, 100);
-	randomSeed(fxrand() * 10000);
-	noiseSeed(fxrand() * 10000);
-	rectMode(CENTER);
-	angleMode(DEGREES);
-	background(50, 10, 10);
 }
-
 function draw() {
 	noLoop();
 }
