fcef4ac1910affac283987f5addcfcce18ddf1fd
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2024-09-09 22:01:14 -0400

    new animation settings

--- project/public/object/mover.js
+++ project/public/object/mover.js
@@ -1,12 +1,12 @@
 class Mover {
-	constructor(x, y, hue, scl1, scl2, ang1, ang2, xMin, xMax, yMin, yMax, isBordered, seed) {
+	constructor(x, y, xi, yi, hue, scl1, scl2, ang1, ang2, xMin, xMax, yMin, yMax, isBordered, seed) {
 		this.x = x;
 		this.y = y;
 		this.initHue = hue;
 		this.initSat = random([0, 0, 10, 20, 20, 40, 50, 60, 70, 80, 80, 90, 90, 100]);
 		this.initBri = random([10, 10, 20, 20, 40, 50, 60, 70, 80, 80, 90, 100]);
 		this.initAlpha = 100;
-		this.initS = 0.5;
+		this.initS = 0.4;
 		this.hue = this.initHue;
 		this.sat = this.initSat;
 		this.bri = this.initBri;
@@ -25,6 +25,8 @@ class Mover {
 		this.xMax = xMax;
 		this.yMin = yMin;
 		this.yMax = yMax;
+		this.xi = xi;
+		this.yi = yi;
 		this.isBordered = isBordered;
 	}
 
@@ -38,10 +40,9 @@ class Mover {
 	}
 
 	move() {
-		let p = superCurve(this.x, this.y, this.scl1, this.scl2, this.ang1, this.ang2, this.seed);
-
+		let p = superCurve(this.x, this.y, this.scl1, this.scl2, this.ang1, this.ang2, this.seed, this.xi, this.yi);
 		this.xRandDivider = random(2.001, 2.1);
-		this.yRandDivider = random(2.001, 2.1);
+		this.yRandDivider = random(1.001, 1.1);
 		this.xRandSkipper = random(-1.1, 1.1);
 		this.yRandSkipper = random(-1.1, 1.1);
 
@@ -49,8 +50,6 @@ class Mover {
 		this.y += p.y / this.yRandDivider + this.yRandSkipper;
 
 		//shortand for if this.x is less than 0, set this.x to width and vice versa
-		this.x = this.x < 0 ? width : this.x > width ? 0 : this.x;
-		this.y = this.y < 0 ? height : this.y > height ? 0 : this.y;
 
 		let pxy = p.x - p.y;
 
@@ -63,7 +62,7 @@ class Mover {
 		this.hue = this.hue < 0 ? this.hue + 360 : this.hue > 360 ? this.hue - 360 : this.hue;
 
 		//this.a = map(p.x, -4, 4, this.initAlpha - 5, this.initAlpha + 5, true);
-		this.s = map(p.x, -4, 4, this.initS + 0.4, this.initS - 0.4, true);
+		this.s = map(p.x, -4, 4, this.initS + 0.8, this.initS, true);
 
 		//this.hue = this.hue > 360 ? this.hue - 360 : this.hue < 0 ? this.hue + 360 : this.hue;
 		/* 		this.sat = map(p.x, -4, 4, 0, 100, true);
@@ -86,9 +85,9 @@ class Mover {
 	}
 }
 
-function superCurve(x, y, scl1, scl2, ang1, ang2, seed) {
-	let nx = x,
-		ny = y,
+function superCurve(x, y, scl1, scl2, ang1, ang2, seed, xi, yi) {
+	let nx = x + xi,
+		ny = y + yi,
 		a1 = ang1,
 		a2 = ang2,
 		scale1 = scl1,
@@ -96,23 +95,23 @@ function superCurve(x, y, scl1, scl2, ang1, ang2, seed) {
 		dx,
 		dy;
 
-	dx = oct6(nx, ny, scale1, 0);
-	dy = oct6(nx, ny, scale2, 2);
+	dx = oct(nx, ny, scale1, 0, 6);
+	dy = oct(nx, ny, scale2, 2, 6);
 	nx += dx * a1;
 	ny += dy * a2;
 
-	dx = oct6(nx, ny, scale1, 1);
-	dy = oct6(nx, ny, scale2, 3);
+	dx = oct(nx, ny, scale1, 1, 6);
+	dy = oct(nx, ny, scale2, 3, 6);
 	nx += dx * a1;
 	ny += dy * a2;
 
-	dx = oct6(nx, ny, scale1, 1);
-	dy = oct6(nx, ny, scale2, 1);
+	dx = oct(nx, ny, scale1, 1, 6);
+	dy = oct(nx, ny, scale2, 1, 6);
 	nx += dx * a1;
 	ny += dy * a2;
 
-	let un = oct6(nx, ny, scale1, 3);
-	let vn = oct6(nx, ny, scale2, 2);
+	let un = oct(nx, ny, scale1, 3, 6);
+	let vn = oct(nx, ny, scale2, 2, 6);
 
 	let u = map(un, -0.5, 0.5, -4, 4, true);
 	let v = map(vn, -0.5, 0.5, -4, 4, true);
--- project/public/sketch.js
+++ project/public/sketch.js
@@ -1,4 +1,4 @@
-let features = '';
+let features = "";
 let movers = [];
 let scl1;
 let scl2;
@@ -10,12 +10,29 @@ let xMin;
 let xMax;
 let yMin;
 let yMax;
-let isBordered = false;
+let isBordered = true;
 let drawing = false;
-let scl1Off = 0.00001,
-	scl2Off = 0.00001,
-	ang1Off = 0,
-	ang2Off = 0;
+let hue = Math.random() * 360;
+let easeAng = 0,
+	easeScalar = 0.001,
+	easeScalar2 = 200,
+	cycleCount = 0,
+	xi = -600,
+	yi = -600,
+	xoff = Math.random() * 10000,
+	yoff = Math.random() * 10000,
+	axoff = Math.random() * 10000,
+	ayoff = Math.random() * 10000,
+	sxoff = Math.random() * 10000,
+	syoff = Math.random() * 10000;
+({sin, cos, imul, PI} = Math);
+TAU = PI * 2;
+F = (N, f) => [...Array(N)].map((_, i) => f(i));
+
+let video_duration = 10; //seconds
+let video_fps = 25;
+let current_video_frame = 0;
+let total_video_frames = video_duration * video_fps;
 
 /* P5Capture.setDefaultOptions({
 	format: 'mp4',
@@ -37,71 +54,101 @@ function setup() {
 		pixelDensity(1.0);
 	}
 	//createCanvas((16 * 300) / 3, (16 * 300) / 3);
-	createCanvas(1080, 1920);
+	createCanvas(1080, 1980);
 	colorMode(HSB, 360, 100, 100, 100);
 	rseed = randomSeed(fxrand() * 10000);
 	nseed = noiseSeed(fxrand() * 10000);
+
+	scl1 = random(0.0025, 0.0025);
+	scl2 = scl1;
+	ang1 = int(random(2500, 4000));
+	ang2 = int(random(2500, 4000));
 
 	INIT(rseed);
 }
 
 function draw() {
 	// put drawing code here
+	//blendMode(SCREEN);
 	for (let i = 0; i < movers.length; i++) {
 		movers[i].show();
 		movers[i].move();
 	}
+	blendMode(BLEND);
 
 	// every 100 frames, save the canvas
-	if (scl2 < 0.005) {
-		if (frameCount % 100 == 0) {
-			console.log('done');
-			scl1Off *= 1.1;
-			scl2Off *= 1.1;
+
+	if (frameCount % 75 == 0) {
+		let cosIndex = cos(radians(easeAng));
+		console.log("cosIndex: " + cosIndex);
+		if (current_video_frame < total_video_frames) {
+			current_video_frame++;
+			movers = [];
 			saveArtwork();
+			elapsedTime = 0;
+			frameCount = 0;
 			INIT(rseed);
-			//noLoop();
+		} else {
+			noLoop();
 		}
-	} else {
-		noLoop();
+		console.log("cycleCount: " + cycleCount);
 	}
 }
 
-function windowResized() {
+/* function windowResized() {
 	resizeCanvas(windowWidth, windowHeight);
 	INIT(rseed);
 }
-
+ */
 function INIT(seed) {
 	movers = [];
+	let easing = easeAng;
+	let cosEasing = cos(easing);
+	//scl1 += map(noise(sxoff, syoff), 0, 1, -0.0001, 0.0001, true);
+	scl1 = map(cosEasing, -1, 1, 0.00041, 0.00004, true);
+	scl2 = scl1;
 
-	scl1 = random(0.00001, 0.005);
-	scl2 = random(0.00001, 0.005);
-	ang1 = int(random(1000));
-	ang2 = int(random(1000));
-	scl1 = 0.00053 + scl1Off;
-	scl2 = 0.00053 + scl2Off;
-	ang1 = 952;
-	ang2 = 688;
+	angle1 = int(map(noise(axoff, ayoff), 0, 1, ang1 * 2, ang1 * 2, true));
+	angle2 = int(map(noise(ayoff, axoff), 0, 1, ang1 * 2, ang2 * 2, true));
+	//angle1 = int(map(cos(easing), -1, 1, 0, 2000, true));
+	//xi = map(cosEasing, -1, 1, -600, -600, true);
+	xi = map(cosEasing, -1, 1, -300, -600, true);
+	yi = map(cosEasing, -1, 1, -600, -600, true);
+	//hue += map(noise(xoff, yoff), 0, 1, -2, 2, true);
+	//hue < 0 ? hue + 360 : hue > 360 ? hue - 360 : hue;
 
-	console.log('scl1: ' + scl1);
-	console.log('scl2: ' + scl2);
-	console.log('ang1: ' + ang1);
-	console.log('ang2: ' + ang2);
+	console.log("xi: " + xi);
+	console.log("yi: " + yi);
 
-	xMin = 0.2;
-	xMax = 0.8;
-	yMin = 0.15;
-	yMax = 0.85;
-	/* 	xMin = -0.05;
-	xMax = 1.05;
-	yMin = -0.05;
-	yMax = 1.05; */
+	// easeAng increment should make the easing function complete a full cycle in the total_video_frames
+	let easeAngIncrement = PI / (total_video_frames / 1);
+	easeAng += easeAngIncrement;
+	console.log("easeAng: " + easeAng);
+	console.log("easeAngIncrement: " + easeAngIncrement);
+	console.log("current_video_frame: " + current_video_frame);
+	xoff += 0.001;
+	yoff += 0.001;
+	axoff += 0.01;
+	ayoff += 0.01;
+	sxoff += 0.01;
+	syoff += 0.01;
 
-	let hue = random(360);
+	console.log("scl1: " + scl1);
+	console.log("scl2: " + scl2);
+	console.log("ang1: " + angle1);
+	console.log("ang2: " + angle2);
+	console.log("cos(easing): " + cosEasing);
+
+	xMin = -0.1;
+	xMax = 1.1;
+	yMin = -0.1;
+	yMax = 1.1;
+	/* 	xMin = -0.15;
+	xMax = 1.15;
+	yMin = -0.15;
+	yMax = 1.15; */
 	hue = 71.11800734885037;
-	console.log('hue: ' + hue);
-	for (let i = 0; i < 40000; i++) {
+	for (let i = 0; i < 70000; i++) {
 		/* 		// distribue the movers within a circle using polar coordinates
 		let r = randomGaussian(4, 2);
 		let theta = random(0, TWO_PI);
@@ -113,8 +160,8 @@ function INIT(seed) {
 
 		let initHue = hue + random(-10, 10);
 		initHue = initHue > 360 ? initHue - 360 : initHue < 0 ? initHue + 360 : initHue;
-		movers.push(new Mover(x, y, initHue, scl1, scl2, ang1, ang2, xMin, xMax, yMin, yMax, isBordered, seed));
+		movers.push(new Mover(x, y, xi, yi, initHue, scl1, scl2, angle1, angle2, xMin, xMax, yMin, yMax, isBordered, easing));
 	}
-	let bgCol = spectral.mix('#fff', '#000', 0.138);
+	let bgCol = spectral.mix("#000", "#fff", 0.91);
 	background(bgCol);
 }
