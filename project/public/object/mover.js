e805d7a407ccb81173910c5dcdf70fb3516250dc
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2024-11-07 00:23:34 +0100

    v1.1

--- project/public/object/mover.js
+++ project/public/object/mover.js
@@ -7,7 +7,7 @@ class Mover {
 		//this.initBri = random([0, 0, 10, 20]);
 		this.initBri = random([0, 10, 20, 30, 40, 50, 60, 70, 70, 70, 80, 80, 90, 100]);
 		this.initAlpha = 100;
-		this.initS = 0.4 * MULTIPLIER;
+		this.initS = 0.5 * MULTIPLIER;
 		this.s = this.initS;
 		this.hue = this.initHue;
 		this.sat = this.initSat;
@@ -61,7 +61,7 @@ class Mover {
 
 		//! CHECK WHY ANG AND SCL IS NOT AGNOSTIC TO MULTIPLIER
 		this.ang1 = int(map(distCircle, -1400, 0, 1, 3000, true));
-		this.ang2 = int(map(distCircle, -1400, 0, 3000, 1, true));
+		this.ang2 = int(map(distCircle, -1400, 0, 1, 3000, true));
 		this.scl1 = map(distCircle, -1300, -2, 0.05, 0.003, true);
 		this.scl2 = map(distCircle, -1300, -2, 0.002, 0.001, true);
 		//! CHECK WHY ANG AND SCL IS NOT AGNOSTIC TO MULTIPLIER
@@ -78,7 +78,7 @@ class Mover {
 		this.sat += map(p.x, -this.uvalue, 1, -this.satStep, this.satStep, true);
 		this.sat = this.sat > 100 ? 100 : this.sat < 0 ? 0 : this.sat;
 		this.bri += map(p.y, -1, this.uvalue, this.briStep, -this.briStep, true);
-		this.bri = this.bri > 100 ? 100 : this.bri < 0 ? 0 : this.bri;
+		this.bri = this.bri > 100 ? 0 : this.bri < 0 ? 100 : this.bri;
 
 		this.a = map(abs(distCircle), 0, 200, 100, 0, true);
 
