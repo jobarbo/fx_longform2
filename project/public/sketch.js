15d609c8f3a31daf3a7503f0ac48f6ce8bccec00
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2024-11-12 07:27:52 -0500

    Square ratio

--- project/public/sketch.js
+++ project/public/sketch.js
@@ -13,7 +13,7 @@ function setup() {
 	// canvas setup
 	DIM = min(windowWidth, windowHeight);
 	MULTIPLIER = DIM / DEFAULT_SIZE;
-	c = createCanvas(DIM, DIM * 1.4);
+	c = createCanvas(DIM, DIM * 1);
 	pixelDensity(3);
 	colorMode(HSB, 360, 100, 100, 100);
 	randomSeed(fxrand() * 10000);
