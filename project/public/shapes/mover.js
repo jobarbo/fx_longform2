b39b964b935f60620d17b277c2695c82ba87e20a
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2025-02-14 15:21:50 -0500

    Original Glasswork

--- project/public/shapes/mover.js
+++ project/public/shapes/mover.js
@@ -112,9 +112,9 @@ function superCurve(x, y, scl1, scl2, scl3, sclOff1, sclOff2, sclOff3, xMin, yMi
 		scaleOffset1 = sclOff1,
 		scaleOffset2 = sclOff2,
 		scaleOffset3 = sclOff3,
-		noiseScale1 = 0.14,
-		noiseScale2 = 0.12,
-		noiseScale3 = 0.12;
+		noiseScale1 = 1,
+		noiseScale2 = 1,
+		noiseScale3 = 1;
 
 	un = sin(nx * (scale1 * scaleOffset1) + rseed) + cos(nx * (scale2 * scaleOffset2) + rseed) - sin(nx * (scale3 * scaleOffset3) + rseed);
 	vn = cos(ny * (scale1 * scaleOffset1) + rseed) + sin(ny * (scale2 * scaleOffset2) + rseed) - cos(ny * (scale3 * scaleOffset3) + rseed);
@@ -132,10 +132,10 @@ function superCurve(x, y, scl1, scl2, scl3, sclOff1, sclOff2, sclOff3, xMin, yMi
 	let minV = map(ny, yMin * height, yMax * height, -3, 3, true); */
 
 	//! pNoise x SineCos
-	let maxU = map(oct(ny * (scale1 * scaleOffset1) + rseed, ny * (scale2 * scaleOffset3) + rseed, noiseScale1, 1, 1), -1.5, 1.5, -10, 10, true);
-	let maxV = map(oct(nx * (scale2 * scaleOffset1) + rseed, nx * (scale1 * scaleOffset2) + rseed, noiseScale2, 2, 1), -1.5, 1.5, -10, 10, true);
-	let minU = map(oct(ny * (scale3 * scaleOffset1) + rseed, ny * (scale1 * scaleOffset3) + rseed, noiseScale3, 0, 1), -1.5, 1.5, -10, 10, true);
-	let minV = map(oct(nx * (scale1 * scaleOffset2) + rseed, nx * (scale3 * scaleOffset3) + rseed, noiseScale2, 3, 1), -1.5, 1.5, -10, 10, true);
+	let maxU = map(oct(ny * (scale1 * scaleOffset1) + rseed, ny * (scale2 * scaleOffset3) + rseed, noiseScale1, 1, 1), -0.005, 0.005, -4, 4, true);
+	let maxV = map(oct(nx * (scale2 * scaleOffset1) + rseed, nx * (scale1 * scaleOffset2) + rseed, noiseScale2, 2, 1), -0.005, 0.005, -4, 4, true);
+	let minU = map(oct(ny * (scale3 * scaleOffset1) + rseed, ny * (scale1 * scaleOffset3) + rseed, noiseScale3, 0, 1), -0.005, 0.005, -4, 4, true);
+	let minV = map(oct(nx * (scale1 * scaleOffset2) + rseed, nx * (scale3 * scaleOffset3) + rseed, noiseScale2, 3, 1), -0.005, 0.005, -4, 4, true);
 
 	//! Wobbly noise square and stuff
 	/* 	let maxU = map(noise(ny * (scale1 * scaleOffset1) + nseed), 0, 1, 0, 3, true);
@@ -156,8 +156,8 @@ function superCurve(x, y, scl1, scl2, scl3, sclOff1, sclOff2, sclOff3, xMin, yMi
 	let minV = -0.11; */
 
 	//! Introverted
-	let u = map(vn, map(nx, xMin * width, xMax * width, -2.5, -0.0000001), map(nx, xMin * width, xMax * width, 0.0000001, 2.5), minU, maxU, true);
-	let v = map(un, map(ny, yMin * height, yMax * height, -2.5, -0.0000001), map(ny, yMin * height, yMax * height, 0.0000001, 2.5), minV, maxV, true);
+	let u = map(vn, map(nx, xMin * width, xMax * width, -4.5, -0.0000001), map(nx, xMin * width, xMax * width, 0.0000001, 4.5), minU, maxU, true);
+	let v = map(un, map(ny, yMin * height, yMax * height, -4.5, -0.0000001), map(ny, yMin * height, yMax * height, 0.0000001, 4.5), minV, maxV, true);
 
 	//! Extroverted
 	/* 	let u = map(vn, map(ny, xMin * width, xMax * width, -5.4, -0.0001), map(ny, xMin * width, xMax * width, 0.0001, 5.4), minU, maxU, true);
@@ -166,17 +166,8 @@ function superCurve(x, y, scl1, scl2, scl3, sclOff1, sclOff2, sclOff3, xMin, yMi
 	//! Equilibrium
 	/* 	let u = map(vn, -0.000000000000000001, 0.000000000000000001, minU, maxU, true);
 	let v = map(un, -0.000000000000000001, 0.000000000000000001, minV, maxV, true); */
-
-	let zu = ZZ(u, 30, 15, 0.008) * MULTIPLIER;
-	let zv = ZZ(v, 30, 15, 0.008) * MULTIPLIER;
-
-	//! PAGODA (below is noiseScale and scaleOffset)
-	//! 2
-	//! 0.001
-	//! 2
-	/* 	let zu = ZZ(u, 35, 40, 0.005) * MULTIPLIER;
-	let zv = ZZ(v, 35, 40, 0.005) * MULTIPLIER; */
-
+	let zu = ZZ(u, 35, 80, 0.018);
+	let zv = ZZ(v, 35, 80, 0.018);
 	let p = createVector(zu, zv);
 	return p;
 }
