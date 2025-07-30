65ed694f6a35389819b773fbb1798a1affa5ddfd
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2025-07-30 13:24:55 -0400

    padding = 0.1

--- project/public/sketch.js
+++ project/public/sketch.js
@@ -407,7 +407,7 @@ function INIT(rseed, nseed) {
 	let amplitude2 = 1 * MULTIPLIER;
 
 	// Simple 10% padding calculation with artwork ratio
-	let padding = 0.4;
+	let padding = 0.1;
 	xMin = padding;
 	xMax = 1 - padding;
 	yMin = padding;
